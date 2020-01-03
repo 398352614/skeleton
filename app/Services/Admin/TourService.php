b511057a661725b0c9749c38c95d7aef659bbdbf
tomtiddler <balloontmz@gmail.com>
2020-01-03 17:11:54 +0800

    test

--- app/Services/Admin/TourService.php
+++ app/Services/Admin/TourService.php
@@ -187,7 +187,7 @@ class TourService extends BaseService
         if ($rowCount === false) {
             throw new BusinessLogicException('车辆分配失败,请重新操作');
         }
-        OrderTrailService::OrderStatusChangeUseOrderIDs(Order::where('tour_no', $tour->tour_no)->pluck('id'), BaseConstService::ORDER_TRAIL_ASSIGN_DRIVER);
+        OrderTrailService::OrderStatusChangeUseOrderCollection(Order::where('tour_no', $tour->tour_no)->get(), BaseConstService::ORDER_TRAIL_ASSIGN_DRIVER);
     }
 
     /**
