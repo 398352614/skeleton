6e8805e854aaf48c058d7d165fcbd31e61a1c5b0
tomtiddler <balloontmz@gmail.com>
2020-01-04 18:47:23 +0800

    car resource 返回修改

--- app/Http/Resources/CarResource.php
+++ app/Http/Resources/CarResource.php
@@ -14,6 +14,8 @@ class CarResource extends JsonResource
      */
     public function toArray($request)
     {
+        $car = parent::toArray($request);
+        $car['brand_name'] = $this->brand->cn_name;
         return parent::toArray($request);
     }
 }
