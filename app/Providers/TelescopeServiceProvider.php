331af865c8172f1654a938dde53dc52d6874a043
HuoZhangqi <h947136@qq.com>
2020-01-06 10:09:09 +0800

    修改Telescope开发环境权限

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -63,9 +63,11 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewTelescope', function ($user) {
-            return in_array($user->email, [
+            /*return in_array($user->email, [
                 //
-            ]);
+            ]);*/
+
+            return true;
         });
     }
 }
