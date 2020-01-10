7029bbe1d9c98ddfcd3fe65731ebc1bec935c8b9
longfangyao <827193289@qq.com>
2020-01-10 17:41:02 +0800

    0 => 4.4677739, 修改

--- app/Traits/LocationTrait.php
+++ app/Traits/LocationTrait.php
@@ -29,10 +29,10 @@ trait LocationTrait
             $result = $client->request('GET', $url, ['http_errors' => false, 'timeout' => 3]);
             $featureList = json_decode((string)($result->getBody()), TRUE)['features'];
         } catch (\Exception $ex) {
-            throw new \Exception('可能由于网络问题，无法获取具体信息，请稍后再尝试');
+            throw new \App\Exceptions\BusinessLogicException('可能由于网络问题，无法获取具体信息，请稍后再尝试');
         }
         $count = count($featureList);
-        if (($count == 0) || ($count > 1)) {
+        if (($count == 0) || ($count > 3)) {
             throw new \App\Exceptions\BusinessLogicException('国家,城市,街道,门牌号或邮编不正确，请仔细检查输入或联系客服');
         }
         return ['lon' => $featureList[0]['geometry']['coordinates'][0], 'lat' => $featureList[0]['geometry']['coordinates'][1]];
