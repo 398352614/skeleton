76010ba0f53a141753c4448e88e1ef26bd68291d
17570715315 <398352614@qq.com>
2020-01-12 16:59:51 +0800

    2

---
+++ database/migrations/2020_01_12_164605_alter_car_add_columns_lisence_government_material.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterCarAddColumnsLisenceGovernmentMaterial extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('driver', function (Blueprint $table) {
+            $table->string('lisence_material_name', 250)->default('')->nullable()->after('lisence_material')->comment('驾照材料名');
+            $table->string('government_material_name',250)->default('')->nullable()->after('government_material')->comment('政府材料名');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('driver', function (Blueprint $table) {
+            $table->dropColumn('lisence_material_name');
+            $table->dropColumn('government_material_name');
+        });
+    }
+}
