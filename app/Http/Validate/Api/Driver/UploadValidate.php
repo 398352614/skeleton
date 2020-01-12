b1a49d9d09b790e06a8682af4653e1b64b391fc6
17570715315 <398352614@qq.com>
2020-01-12 15:51:57 +0800

    pdf

--- app/Http/Validate/Api/Driver/UploadValidate.php
+++ app/Http/Validate/Api/Driver/UploadValidate.php
@@ -22,7 +22,7 @@ class UploadValidate extends BaseValidate
 
     public $rules = [
         'image' => 'required|image|mimes:jpeg,bmp,png',
-        'file' => 'required|file|mimes:txt,xls,xlsx,doc,docx,jpeg,bmp,png',
+        'file' => 'required|file|mimes:txt,xls,xlsx,doc,docx,jpeg,bmp,png,pdf',
         'dir' => 'required|string|max:20'
     ];
 
@@ -35,7 +35,7 @@ class UploadValidate extends BaseValidate
         'image.image' => '必须是图片',
         'image.mimes' => ':attribute类型必须是jpeg,bmp,png类型',
         'file.file' => '必须是文件',
-        'file.mimes' => ':attribute类型必须是txt,excel,word,jpeg,bmp,png类型',
+        'file.mimes' => ':attribute类型必须是txt,excel,word,jpeg,bmp,png,pdf类型',
 
     ];
 }
