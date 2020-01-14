21e18b5c1ebca26bd852c380af841dbeb2d44c4c
longfangyao <827193289@qq.com>
2020-01-14 14:01:36 +0800

    订单规则调整

--- app/Http/Controllers/Api/Admin/RegisterController.php
+++ app/Http/Controllers/Api/Admin/RegisterController.php
@@ -87,7 +87,7 @@ class RegisterController extends BaseController
                 'type' => $value,
                 'prefix' => substr('000'.$company->id, -4, 4),
                 'start_index' => 1,
-                'length' => 13,
+                'length' => ($value == BaseConstService::ORDER_NO_TYPE) ? 13 : 4,
             ];
         }, $rules);
 
--- app/Services/OrderNoRuleService.php
+++ app/Services/OrderNoRuleService.php
@@ -53,8 +53,13 @@ class OrderNoRuleService extends BaseService
             throw new BusinessLogicException('单号规则不存在,请先添加单号规则');
         }
         $info = $info->toArray();
-        $orderNo = BaseConstService::BATCH . $info['prefix'] . sprintf("%0{$info['length']}s", $info['start_index']);
-        $rowCount = parent::updateById($info['id'], ['start_index' => $info['start_index'] + 1]);
+        //获取开始索引
+        $index = ($info['end_alpha'] === 'Z') ? $info['start_index'] + 1 : $info['start_index'];
+        //获取尾号字母
+        $endAlpha = \AlphaTrait::getNextUpAlpha($info['end_alpha']);
+        //生成单号
+        $orderNo = BaseConstService::BATCH . $info['prefix'] . sprintf("%0{$info['length']}s%s", $index, $endAlpha);
+        $rowCount = parent::updateById($info['id'], ['start_index' => $index, 'end_alpha' => $endAlpha]);
         if ($rowCount === false) {
             throw new BusinessLogicException('单号生成失败,请重新操作');
         }
@@ -73,9 +78,13 @@ class OrderNoRuleService extends BaseService
         if (empty($info)) {
             throw new BusinessLogicException('单号规则不存在,请先添加单号规则');
         }
-        $info = $info->toArray();
-        $orderNo = BaseConstService::TOUR . $info['prefix'] . sprintf("%0{$info['length']}s", $info['start_index']);
-        $rowCount = parent::updateById($info['id'], ['start_index' => $info['start_index'] + 1]);
+        //获取开始索引
+        $index = ($info['end_alpha'] === 'Z') ? $info['start_index'] + 1 : $info['start_index'];
+        //获取尾号字母
+        $endAlpha = \AlphaTrait::getNextUpAlpha($info['end_alpha']);
+        //生成单号
+        $orderNo = BaseConstService::TOUR . $info['prefix'] . sprintf("%0{$info['length']}s%s", $index, $endAlpha);
+        $rowCount = parent::updateById($info['id'], ['start_index' => $index, 'end_alpha' => $endAlpha]);
         if ($rowCount === false) {
             throw new BusinessLogicException('单号生成失败,请重新操作');
         }
