5ab371549485561adf62f9c8dec4ef95ea2849bc
17570715315 <398352614@qq.com>
2020-02-04 14:03:54 +0800

    员工详情DEBUG2

--- app/Http/Resources/EmployeeListResource.php
+++ app/Http/Resources/EmployeeListResource.php
@@ -8,6 +8,21 @@ class EmployeeListResource extends JsonResource
 {
     public function toArray($request)
     {
+        if(empty($this->institution_id)){
+            return [
+                'id' => $this->id ?? '',
+                'email' => $this->email,
+                'fullname' => $this->fullname,
+                'username' => $this->username,
+                'phone' => $this->phone ?? '',
+                'remark' => $this->remark ?? '',
+                'group' => $this->group ?? '',
+                'institution' => '',
+                'forbid_login' => $this->forbid_login,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+            ];
+        }
         return [
             'id' => $this->id ?? '',
             'email' => $this->email,
