abd10cadbc5cc6841a94533e0ad7a7b1d2fb73a3
tomtiddler <balloontmz@gmail.com>
2020-02-11 16:34:10 +0800

    日志

--- app/Services/BaseServices/XLDirectionService.php
+++ app/Services/BaseServices/XLDirectionService.php
@@ -29,7 +29,7 @@ class XLDirectionService
             'NLE19901212!'
         ]);
         // $this->auth = [
-            
+
         // ];
     }
 
@@ -40,6 +40,7 @@ class XLDirectionService
     {
         //传入的数据必须是由 code,latitude,longitude 三个元素组成的数组构成的二维数组
         $locSeq = [];
+        app('log')->debug('获取优化线路传递的参数为:', $data ?? []);
         foreach ($data as $key => $loc) {
             $temp = [];
             $temp['name'] = $loc['batch_no'];
@@ -52,8 +53,8 @@ class XLDirectionService
 
     public function getTour(array $locSeq): array
     {
-        $resp = $this->curl->post(self::BASE_URL . 'tour/', ['locations'=>$locSeq], 0);
-        if(!$resp || !$resp['feasible']) {
+        $resp = $this->curl->post(self::BASE_URL . 'tour/', ['locations' => $locSeq], 0);
+        if (!$resp || !$resp['feasible']) {
             app('log')->info('线路规划失败或者不可靠');
             return null;
         }
