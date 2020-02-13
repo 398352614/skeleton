cb6e130450aad064e51fdadea81f4c6763f20eae
zhuyi <695433846@qq.com>
2020-02-13 14:18:45 +0800

    修改

--- database/migrations/2020_02_12_111549_create_company_config_table.php
+++ database/migrations/2020_02_12_111549_create_company_config_table.php
@@ -16,11 +16,11 @@ class CreateCompanyConfigTable extends Migration
         Schema::create('company_config', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('company_id')->default(null)->nullable()->comment('公司ID');
-            $table->string('line_rule', 50)->default('')->nullable()->comment('配置名称');
-            $table->string('weight_unit', 50)->default('')->nullable()->comment('配置值');
-            $table->string('currency_unit', 50)->default('')->nullable()->comment('配置值');
-            $table->string('volume_unit', 50)->default('')->nullable()->comment('配置值');
-            $table->string('map', 50)->default('')->nullable()->comment('配置值');
+            $table->string('line_rule', 50)->default('')->nullable()->comment('线路分配规则');
+            $table->string('weight_unit', 50)->default('')->nullable()->comment('重量单位');
+            $table->string('currency_unit', 50)->default('')->nullable()->comment('货币单位');
+            $table->string('volume_unit', 50)->default('')->nullable()->comment('体积单位');
+            $table->string('map', 50)->default('')->nullable()->comment('地图引擎');
             $table->dateTime('created_at')->default(null)->nullable()->comment('创建时间');
             $table->dateTime('updated_at')->default(null)->nullable()->comment('修改时间');
         });
