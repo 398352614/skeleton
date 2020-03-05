e03d91fd51e1815bef88ee6587c038ad08eaaef2
longfangyao <827193289@qq.com>
2020-03-05 13:37:02 +0800

    修改

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Services\CurlClient;
 use App\Services\GoogleApiService;
 use App\Services\ReisService;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
@@ -17,23 +18,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('reis', function($app){
+        $this->app->singleton('reis', function ($app) {
             return new ReisService($app);
         });
 
-        $this->app->singleton('curl', function($app){
+        $this->app->singleton('curl', function ($app) {
             return new CurlClient($app);
         });
 
-        $this->app->bind(CurlClient::class, function($app){
+        $this->app->bind(CurlClient::class, function ($app) {
             return new CurlClient($app);
         });
 
-        $this->app->bind(ReisService::class, function($app){
+        $this->app->bind(ReisService::class, function ($app) {
             return new ReisService($app);
         });
 
-        $this->app->bind(GoogleApiService::class, function($app){
+        $this->app->bind(GoogleApiService::class, function ($app) {
             return new GoogleApiService($app);
         });
 
@@ -49,6 +50,35 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') === 'local') {
+            DB::listen(
+                function ($sql) {
+                    foreach ($sql->bindings as $i => $binding) {
+                        if ($binding instanceof \DateTime) {
+                            $sql->bindings[$i] = $binding->format('\'Y-m-d H:i:s\'');
+                        } else {
+                            if (is_string($binding)) {
+                                $sql->bindings[$i] = "'$binding'";
+                            }
+                        }
+                    }
+
+                    // Insert bindings into query
+                    $query = str_replace(array('%', '?'), array('%%', '%s'), $sql->sql);
+
+                    $query = vsprintf($query, $sql->bindings);
+
+                    // Save the query to file
+                    $logFile = fopen(
+                        storage_path('logs' . DIRECTORY_SEPARATOR . date('Y-m-d') . '_query.log'),
+                        'a+'
+                    );
+                    fwrite($logFile, date('Y-m-d H:i:s') . ': ' . $query . PHP_EOL);
+                    fclose($logFile);
+                }
+            );
+        }
+
         //验证类扩展
         Validator::extend('uniqueIgnore', 'App\\Http\\Validate\\BaseValidate@uniqueIgnore');
     }
