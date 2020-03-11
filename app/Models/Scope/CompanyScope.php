aef433a2c8802d65f12f82b8b67e71a3c0f148b1
longfangyao <827193289@qq.com>
2020-03-11 18:36:20 +0800

    修改

--- app/Models/Scope/CompanyScope.php
+++ app/Models/Scope/CompanyScope.php
@@ -67,7 +67,7 @@ class CompanyScope implements Scope
         //如果是商家端
         if ($user instanceof Merchant) {
             $builder->whereRaw($model->getTable() . '.company_id' . ' = ' . $user->company_id);
-            if (!($model instanceof Batch) && (!($model instanceof Tour) && !($model instanceof Line) && !($model instanceof LineRange))
+            if (!($model instanceof Batch) && !($model instanceof Tour) && !($model instanceof Line) && !($model instanceof LineRange)
             ) {
                 $builder->whereRaw($model->getTable() . '.merchant_id' . ' = ' . $user->merchant_id);
             }
