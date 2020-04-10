3c384d49a281f3900d783a6dbe8b7293b6ac8078
longfangyao <827193289@qq.com>
2020-04-10 16:37:16 +0800

    修改

--- app/Events/TourNotify/NextBatch.php
+++ app/Events/TourNotify/NextBatch.php
@@ -11,6 +11,7 @@ namespace App\Events\TourNotify;
 use App\Events\Interfaces\ATourNotify;
 use App\Models\Order;
 use App\Services\BaseConstService;
+use Illuminate\Support\Facades\Log;
 
 class NextBatch extends ATourNotify
 {
@@ -28,14 +29,17 @@ class NextBatch extends ATourNotify
     public function getDataList(): array
     {
         $orderList = collect($this->orderList)->groupBy('merchant_id')->toArray();
+        Log::info('order-list:' . json_encode($orderList));
         $batchList = [];
         foreach ($orderList as $merchantId => $merchantOrderList) {
             $batchList[$merchantId] = array_merge($this->batch, ['merchant_id' => $merchantId, 'order_list' => $merchantOrderList]);
         }
+        Log::info('batch-list:' . json_encode($batchList));
         $tourList = [];
         foreach ($batchList as $merchantId => $batch) {
             $tourList[$merchantId] = array_merge($this->tour, ['merchant_id' => $merchantId, 'batch' => $batch]);
         }
+        Log::info('tour-list:' . json_encode($tourList));
         return $tourList;
     }
 
