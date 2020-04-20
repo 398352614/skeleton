1e2ce0c0b73408e39275616cdda8d9aa78732505
longfangyao <827193289@qq.com>
2020-04-20 16:26:14 +0800

    修改

--- database/migrations/2020_04_20_134518_alter_order_add_columns_source.php
+++ database/migrations/2020_04_20_134518_alter_order_add_columns_source.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class AlterOrderAddColumnsSource extends Migration
 {
@@ -14,8 +15,7 @@ class AlterOrderAddColumnsSource extends Migration
     public function up()
     {
         Schema::table('order', function (Blueprint $table) {
-            $table->dropColumn('source');
-            $table->smallInteger('source')->default(1)->nullable()->after('type')->comment('来源1-手动添加2-批量导入3-第三方');
+            DB::statement("alter table `order` MODIFY  COLUMN `source` INT(6) DEFAULT 1 COMMENT '来源'");
             $table->smallInteger('list_mode')->default(1)->nullable()->after('source')->comment('清单模式1-简易模式2-列表模式');
         });
     }
