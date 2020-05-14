f94e2bb1333282b88c7ac0edb6d7669958929902
17570715315 <398352614@qq.com>
2020-05-14 15:44:01 +0800

    可选日期debug5

--- app/Services/Admin/BatchExceptionService.php
+++ app/Services/Admin/BatchExceptionService.php
@@ -22,6 +22,7 @@ class BatchExceptionService extends BaseService
     public $filterRules = [
         'status' => ['=', 'status'],
         'batch_exception_no' => ['like', 'keyword'],
+        'batch_no' => ['like', 'keyword'],
         'created_at' => ['between', ['begin_date', 'end_date']]
     ];
 
