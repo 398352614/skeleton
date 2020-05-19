dc2286c4d6b559302dfd2dffbcdfc70d4672b563
17570715315 <398352614@qq.com>
2020-05-19 19:07:34 +0800

    车辆追踪9

--- app/Models/RouteTracking.php
+++ app/Models/RouteTracking.php
@@ -51,6 +51,8 @@ class RouteTracking extends BaseModel
         'lat',
         'tour_no',
         'driver_id',
+        'time',
+        'route_tracking_id',
         'created_at',
         'updated_at',
     ];
