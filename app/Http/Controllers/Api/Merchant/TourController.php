9d7240df40aa0384645ba6a8dbf8615f6053bdd6
17570715315 <398352614@qq.com>
2020-05-28 11:38:15 +0800

    司机国家debug1

--- app/Http/Controllers/Api/Merchant/TourController.php
+++ app/Http/Controllers/Api/Merchant/TourController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api\Merchant;
 
 use App\Http\Controllers\BaseController;
-use App\Services\Admin\TourService;
+use App\Services\Merchant\TourService;
 use App\Services\Traits\TourRedisLockTrait;
 use Illuminate\Http\Request;
 
