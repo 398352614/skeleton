dd19bcf1850d71cbdafedc5acb60d92795a45fe7
17570715315 <398352614@qq.com>
2020-06-02 15:21:56 +0800

    分配至站点区域debug7

--- app/Services/Admin/RouteTrackingService.php
+++ app/Services/Admin/RouteTrackingService.php
@@ -73,7 +73,7 @@ class RouteTrackingService extends BaseService
                         $routeTracking[$i]['event']=array_merge($routeTracking[$i]['event'],$routeTracking[$i-1]['event']);
                         $info[$i]=Arr::except($routeTracking[$i],['stopTime','created_at','updated_at','time','tour_driver_event_id','driver_id']);
                         $info=Arr::except($info,[$i-1]);
-                        $info[$i]['event']=array_merge([collect($info[$i]['event'])->groupBy('type')->sortByDesc('time')['stop'][0]],collect($info[$i]['event'])->groupBy('type')->toArray()['station']);
+                        $info[$i]['event']=array_merge([collect($info[$i]['event'])->groupBy('type')->sortByDesc('time')['stop'][0]],collect($info[$i]['event'])->groupBy('type')->toArray()['station'] ?? []);
                     }else{
                         $info[$i]=Arr::except($routeTracking[$i],['stopTime','created_at','updated_at','time','tour_driver_event_id','driver_id']);
                     }
