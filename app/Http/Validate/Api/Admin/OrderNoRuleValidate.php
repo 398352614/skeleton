1d08aa064f4b395d2bdea6ebb270577a3a2daf06
longshuang <827193289@qq.com>
2020-06-03 12:02:21 +0800

    update

--- app/Http/Validate/Api/Admin/OrderNoRuleValidate.php
+++ app/Http/Validate/Api/Admin/OrderNoRuleValidate.php
@@ -10,8 +10,8 @@ class OrderNoRuleValidate extends BaseValidate
     public $rules = [
         'type' => 'required|string|max:50',
         'prefix' => 'required|string|max:10|uniqueIgnore:order_no_rule,id',
-        'string_length' => 'nullable|integer|max:6',
-        'int_length' => 'required|integer|max:4',
+        'string_length' => 'nullable|integer',
+        'int_length' => 'required|integer|min:1',
         'status' => 'required|integer|in:1,2',
     ];
 
