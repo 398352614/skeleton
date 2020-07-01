64320d9be3eec2688bb539b1967dbb6817bf7b6c
longshuang <827193289@qq.com>
2020-07-01 16:38:44 +0800

    update

--- app/Services/Merchant/TourService.php
+++ app/Services/Merchant/TourService.php
@@ -456,8 +456,12 @@ class TourService extends BaseService
             $this->query->where('tour_no', $tourNo);
         }
         //若不存在取件线路或者超过最大订单量,则新建取件线路
-        $this->query->where(DB::raw('expect_pickup_quantity+' . intval($batch['expect_pickup_quantity'])), '<=', $line['pickup_max_count']);
-        $this->query->where(DB::raw('expect_pie_quantity+' . intval($batch['expect_pie_quantity'])), '<=', $line['pie_max_count']);
+        if(intval($batch['expect_pickup_quantity']) > 0){
+            $this->query->where(DB::raw('expect_pickup_quantity+' . intval($batch['expect_pickup_quantity'])), '<=', $line['pickup_max_count']);
+        }
+        if(intval($batch['expect_pie_quantity']) > 0){
+            $this->query->where(DB::raw('expect_pie_quantity+' . intval($batch['expect_pie_quantity'])), '<=', $line['pie_max_count']);
+        }
         $where = ['line_id' => $line['id'], 'execution_date' => $batch['execution_date'], 'status' => ['in', [BaseConstService::TOUR_STATUS_1, BaseConstService::TOUR_STATUS_2]]];
         $tour = ($isLock === true) ? parent::getInfoLock($where, ['*'], false) : parent::getInfo($where, ['*'], false);
         return !empty($tour) ? $tour->toArray() : [];
