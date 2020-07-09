911f7f972e4f7446108834958347ae8e25c1311f
longshuang <827193289@qq.com>
2020-07-09 19:50:45 +0800

    update

--- app/Services/BaseService.php
+++ app/Services/BaseService.php
@@ -226,7 +226,6 @@ class BaseService
         SearchTrait::buildQuery($this->query, $where);
         $data = $this->query->first($selectFields);
         if (empty($data)) {
-            $sql = $this->query->toSql();
             $this->query = $this->model::query();
             return [];
         };
