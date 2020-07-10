8f419eadb1fcd5ee40a3d63837741474fa332cb8
longshuang <827193289@qq.com>
2020-07-10 19:25:02 +0800

    update

--- app/Services/Admin/WareHouseService.php
+++ app/Services/Admin/WareHouseService.php
@@ -76,7 +76,7 @@ class WareHouseService extends BaseService
      */
     public function updateById($id, $data)
     {
-        $this->fillData($params);
+        $this->fillData($data);
         $rowCount = parent::updateById($id, $data);
         if ($rowCount === false) {
             throw new BusinessLogicException('仓库修改失败，请重新操作');
