8829dcc61deffc793ededa0f58cc3ec97ec32651
longshuang <827193289@qq.com>
2020-07-16 19:46:48 +0800

    update

--- app/Services/Driver/TourService.php
+++ app/Services/Driver/TourService.php
@@ -715,10 +715,6 @@ class TourService extends BaseService
         !empty($params['material_list']) && $this->dealMaterialList($tour, $params['material_list']);
         /*******************************************1.处理站点下的包裹*************************************************/
         $info = $this->dealPackageList($batch, $params['package_list'] ?? []);
-        
-        $packageList = Package::query()->where('batch_no', $batch['batch_no'])->get(['name', 'order_no', 'express_first_no', 'express_second_no', 'out_order_no', 'expect_quantity', 'actual_quantity', 'status', 'sticker_no', 'sticker_amount'])->toArray();
-        Log::info('assign-batch-begin-package_list', $packageList);
-
         $totalStickerAmount = $info['totalStickerAmount'];
         $orderStickerAmountList = $info['orderStickerAmount'];
         /****************************************2.处理站点下的所有订单************************************************/
@@ -766,8 +762,6 @@ class TourService extends BaseService
                 throw new BusinessLogicException('签收失败');
             }
         }
-        $packageList = Package::query()->where('batch_no', $batch['batch_no'])->get(['name', 'order_no', 'express_first_no', 'express_second_no', 'out_order_no', 'expect_quantity', 'actual_quantity', 'status', 'sticker_no', 'sticker_amount'])->toArray();
-        Log::info('assign-batch-middle-package_list', $packageList);
         /********************************************3.处理站点********************************************************/
         $batchData = [
             'status' => BaseConstService::BATCH_CHECKOUT,
@@ -793,9 +787,6 @@ class TourService extends BaseService
         }
         //重新统计金额
         $this->reCountActualAmountByNo($tour['tour_no']);
-
-        $packageList = Package::query()->where('batch_no', $batch['batch_no'])->get(['name', 'order_no', 'express_first_no', 'express_second_no', 'out_order_no', 'expect_quantity', 'actual_quantity', 'status', 'sticker_no', 'sticker_amount'])->toArray();
-        Log::info('assign-batch-end-package_list', $packageList);
 
         TourTrait::afterBatchSign($tour, $batch);
     }
