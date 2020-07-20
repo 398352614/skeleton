8e5c4e9b3394741d00946aa3d88288860a74e5bf
whoyummy <398352614@qq.com>
2020-07-20 16:01:50 +0800

    debug

--- routes/api_merchant_api.php
+++ routes/api_merchant_api.php
@@ -22,5 +22,6 @@ Route::namespace('Api\Merchant')->middleware(['auth:merchant_api'])->group(funct
     Route::post('order-out-status', 'OrderController@updateOutStatus');//出库
     Route::post('post-code-date-list', 'LineController@getDateListByPostCode');//获取可选日期
     Route::post('order-dispatch-info', 'OrderController@getOrderDispatchInfo');//派送情况
-    Route::post('update-order', 'OrderController@updateByApi');//修改订单
+    Route::post('order-update-phone-date', 'OrderController@updateByApi');//修改订单
+    Route::post('package-info', 'PackageController@showByApi');//包裹查询
 });
