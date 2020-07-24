04c08acc35456ce2003f80955d76ab0710dc3d4c
whoyummy <398352614@qq.com>
2020-07-24 16:12:40 +0800

    仓库预计到达时间建表

---
+++ database/migrations/2020_07_24_154715_alter_tour_add_columns_ware_house_expect_time.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTourAddColumnsWareHouseExpectTime extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tour', function (Blueprint $table) {
+            $table->integer('warehouse_expect_time')->default(0)->after('warehouse_lat')->comment('抵达仓库预计耗时');
+            $table->integer('warehouse_expect_distance')->default(0)->after('warehouse_expect_time')->comment('抵达仓库预计历程');
+            $table->dateTime('warehouse_expect_arrive_time')->default(null)->after('warehouse_expect_distance')->comment('抵达仓库预计时间');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tour', function (Blueprint $table) {
+            $table->dropColumn('warehouse_expect_time');
+            $table->dropColumn('warehouse_expect_distance');
+            $table->dropColumn('warehouse_expect_arrive_time');
+        });
+    }
+}
