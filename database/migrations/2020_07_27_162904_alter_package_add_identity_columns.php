4687363af48d07d48b0f275f71ce55b4bbfd78e3
longshuang <827193289@qq.com>
2020-07-28 10:19:36 +0800

    update

--- database/migrations/2020_07_27_162904_alter_package_add_identity_columns.php
+++ database/migrations/2020_07_27_162904_alter_package_add_identity_columns.php
@@ -15,7 +15,7 @@ class AlterPackageAddIdentityColumns extends Migration
     {
         Schema::table('package', function (Blueprint $table) {
             $table->tinyInteger('is_auth')->default(2)->nullable()->after('remark')->comment('是否需要身份验证1-是2-否');
-            $table->string('auth_fullname', 100)->default('')->nullable()->after('authentication')->comment('身份人姓名');
+            $table->string('auth_fullname', 100)->default('')->nullable()->after('is_auth')->comment('身份人姓名');
             $table->date('auth_birth_date')->default(null)->nullable()->after('auth_fullname')->comment('身份人出身年月');
         });
     }
