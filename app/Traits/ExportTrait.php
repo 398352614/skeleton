21ecd29d98342d2dd1bccbd3fe2d3297b7069a90
whoyummy <398352614@qq.com>
2020-08-11 13:37:47 +0800

    包裹，材料查询2

--- app/Traits/ExportTrait.php
+++ app/Traits/ExportTrait.php
@@ -9,6 +9,7 @@ use App\Exceptions\BusinessLogicException;
 use App\Exports\BaseExport;
 use App\Exports\BatchListExport;
 use App\Exports\MerchantExport;
+use App\Exports\PlanExport;
 use App\Models\Merchant;
 use App\Services\BaseService;
 use App\Traits\ConstTranslateTrait;
@@ -24,10 +25,22 @@ trait ExportTrait
         $this->txtDisk = Storage::disk('admin_image_public');
     }
 
-    public function translate($headings,$dir){
-        for($i=0,$j=count($headings);$i<$j;$i++){
-            $headings[$i]=__('excel.'.$dir.'.'.$headings[$i]);
-        };
+    public function translate($headings, $dir)
+    {
+        if (is_array($headings[0])) {
+            $systemHeadings = array_keys(__('excel.plan'));
+            for ($i = 0, $j = count($headings); $i < $j; $i++) {
+                for ($k = 0, $l = count($headings[$i]); $k < $l; $k++) {
+                    if (in_array($headings[$i][$k], $systemHeadings)) {
+                        $headings[$i][$k] = __('excel.' . $dir . '.' . $headings[$i][$k]);
+                    }
+                }
+            }
+        } else {
+            for ($i = 0, $j = count($headings); $i < $j; $i++) {
+                $headings[$i] = __('excel.' . $dir . '.' . $headings[$i]);
+            }
+        }
         return $headings;
     }
 
@@ -40,22 +53,26 @@ trait ExportTrait
      * @return array
      * @throws BusinessLogicException
      */
-    public function excelExport($name,$headings,$params,$dir)
+    public function excelExport($name, $headings, $params, $dir, $sort = null)
     {
-        $headings=$this->translate($headings,$dir);
+        $headings = $this->translate($headings, $dir);
         $subPath = auth()->user()->company_id . DIRECTORY_SEPARATOR . $dir;
-        $path ='public\\admin\\excel\\'.$subPath . DIRECTORY_SEPARATOR . $name.'.xlsx';
-        try {
-            $rowCount=Excel::store(new BaseExport($params,$headings,$name,$dir),$path);
-        } catch (\Exception $ex) {
+        $path = 'public\\admin\\excel\\' . $subPath . DIRECTORY_SEPARATOR . $name . '.xlsx';
+/*        try {*/
+            if ($dir == 'plan') {
+                $rowCount = Excel::store(new PlanExport($params, $headings, $name, $dir,$sort), $path);
+            } else {
+                $rowCount = Excel::store(new BaseExport($params, $headings, $name, $dir), $path);
+            }
+/*        } catch (\Exception $ex) {
             throw new BusinessLogicException('表格导出失败，请重新操作');
         }
         if ($rowCount === false) {
             throw new BusinessLogicException('表格导出失败，请重新操作');
-        }
+        }*/
         return [
-            'name' => $name.'.xlsx',
-            'path' => Storage::disk('admin_excel_public')->url($subPath . DIRECTORY_SEPARATOR . $name.'.xlsx')
+            'name' => $name . '.xlsx',
+            'path' => Storage::disk('admin_excel_public')->url($subPath . DIRECTORY_SEPARATOR . $name . '.xlsx')
         ];
     }
 
@@ -65,11 +82,12 @@ trait ExportTrait
      * @return array
      * @throws BusinessLogicException
      */
-    public function txtExport($name,$params,$dir){
+    public function txtExport($name, $params, $dir)
+    {
         $subPath = auth()->user()->company_id . DIRECTORY_SEPARATOR . $dir;
-        $params['name'] = date('Ymd') . $params['name'].'.txt';
+        $params['name'] = date('Ymd') . $params['name'] . '.txt';
         try {
-            $rowCount = $this->txtDisk->put($subPath.DIRECTORY_SEPARATOR.$params['name'],$params['txt']);
+            $rowCount = $this->txtDisk->put($subPath . DIRECTORY_SEPARATOR . $params['name'], $params['txt']);
         } catch (\Exception $ex) {
             throw new BusinessLogicException('文档上传失败，请重新操作');
         }
