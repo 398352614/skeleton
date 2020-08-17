0a83e32847ae4730ed39aa837c565247e8633aff
whoyummy <398352614@qq.com>
2020-08-17 21:03:02 +0800

    确认出库

--- app/Services/Admin/ApiTimesService.php
+++ app/Services/Admin/ApiTimesService.php
@@ -4,6 +4,7 @@
 namespace App\Services\Admin;
 
 
+use App\Exceptions\BusinessLogicException;
 use App\Http\Resources\ApiTimesResource;
 use App\Models\ApiTimes;
 use App\Services\BaseService;
@@ -18,12 +19,12 @@ class ApiTimesService extends BaseService
 
     public function __construct(ApiTimes $model)
     {
-        parent::__construct($model,ApiTimesResource::class);
+        parent::__construct($model, ApiTimesResource::class);
     }
 
     public function getPageList()
     {
-        $info['detail_list'] = $this->setFilter()->setOrderBy()->getList([],['*'],false)->toArray();
+        $info['detail_list'] = $this->setFilter()->setOrderBy()->getList([], ['*'], false)->toArray();
         $columns = ['directions_times', 'actual_directions_times', 'api_directions_times', 'distance_times', 'actual_distance_times', 'api_distance_times'];
         foreach ($columns as $v) {
             $info[$v] = 0;
@@ -37,17 +38,21 @@ class ApiTimesService extends BaseService
         return $info;
     }
 
-    public function timesCount($type,$companyId)
+    public function timesCount($type, $companyId)
     {
         $date = Carbon::today()->format('Y-m-d');
-        if (empty(parent::getInfo(['date' => $date, 'company_id' => $companyId], ['*'], false))) {
-            parent::create([
-                'date' => $date,
-                'company_id' => $companyId,
-                $type => 1
-            ]);
-        } else {
-            $this->query->where('date', $date)->where('company_id', $companyId)->increment($type);
+        try {
+            if (empty(parent::getInfo(['date' => $date, 'company_id' => $companyId], ['*'], false))) {
+                parent::create([
+                    'date' => $date,
+                    'company_id' => $companyId,
+                    $type => 1
+                ]);
+            } else {
+                $this->query->where('date', $date)->where('company_id', $companyId)->increment($type);
+            }
+        } catch (\Exception $e) {
         }
+
     }
 }
