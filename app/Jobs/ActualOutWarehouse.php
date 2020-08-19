de9ce9e44d44c16af9e2ec7cbfbe6758e7bad141
longshuang <827193289@qq.com>
2020-08-19 18:41:25 +0800

    update

--- app/Jobs/ActualOutWarehouse.php
+++ app/Jobs/ActualOutWarehouse.php
@@ -107,7 +107,7 @@ class ActualOutWarehouse implements ShouldQueue
             Log::channel('job-daily')->error('智能调度错误:' . $ex->getMessage());
         }
         /**************************************3.通知下一个站点事件************************************************/
-        $tour = DB::table('tour')->where('tour_no', $this->tour_no)->first()->toArray();
+        $tour = Tour::query()->where('tour_no', $this->tour_no)->first()->toArray();
         $nextBatch = TourTrait::getNextBatch($tour['tour_no']);
         if (!empty($nextBatch)) {
             event(new NextBatch($tour, $nextBatch->toArray()));
