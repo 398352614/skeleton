4131d0200c7e19bc4d967bb0742c111950789323
longshuang <827193289@qq.com>
2020-08-27 16:55:24 +0800

    update

--- app/Services/Driver/TourService.php
+++ app/Services/Driver/TourService.php
@@ -243,7 +243,7 @@ class TourService extends BaseService
         }
         $tour = $tour->toArray();
         //查看当前车辆是否已被分配给其他取件线路
-        $otherTour = parent::getInfo(['id' => ['<>', $id], 'car_id' => $params['car_id'], 'execution_date' => $tour['execution_date'], 'status' => ['<>', BaseConstService::TOUR_STATUS_5]], ['*'], false);
+        $otherTour = parent::getInfo(['id' => ['<>', $id], 'car_id' => $params['car_id'], 'execution_date' => $tour['execution_date'], 'status' => ['<>', BaseConstService::TOUR_STATUS_5], 'driver_id' => ['<>', null]], ['*'], false);
         if (!empty($otherTour)) {
             throw new BusinessLogicException('当前车辆已被分配，请选择其他车辆');
         }
