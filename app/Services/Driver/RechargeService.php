54d5ebcacb08e24af4321faa9dd3a90ca1b126b5
whoyummy <398352614@qq.com>
2020-08-28 10:24:12 +0800

    确认出库

--- app/Services/Driver/RechargeService.php
+++ app/Services/Driver/RechargeService.php
@@ -171,6 +171,7 @@ class RechargeService extends BaseService
         }
         $data['data'] = Arr::only(collect($params)->toArray(), ['out_user_id', 'out_user_name', 'merchant_id', 'out_user_name', 'signature', 'recharge_amount', 'recharge_no']);
         $data['data']['driver_name'] = $info['driver_name'];
+        $data['data']['out_user_phone'] = $info['out_user_phone'];
         $data['data']['recharge_screenshot_url'] = $data['data']['signature'];
         $data['data']=Arr::except($data['data'],'signature');
         $data['type'] = 'recharge-process';
