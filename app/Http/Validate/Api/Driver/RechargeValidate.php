aa7c6c828c0d44c3d02415f7f0408b3d9fc7a780
whoyummy <398352614@qq.com>
2020-08-28 13:39:00 +0800

    确认出库

--- app/Http/Validate/Api/Driver/RechargeValidate.php
+++ app/Http/Validate/Api/Driver/RechargeValidate.php
@@ -18,11 +18,11 @@ class RechargeValidate extends BaseValidate
         'out_user_name' => 'required|string',
         'recharge_amount' => 'required|numeric|gte:0.01|lte:2000',
         'recharge_no' => 'required|string|max:250',
-        'recharge_first_pic' => 'required|string|max:250',
+        'recharge_first_pic' => 'nullable|string|max:250',
         'recharge_second_pic' => 'nullable|string|max:250',
         'recharge_third_pic' => 'nullable|string|max:250',
         'signature' => 'required|string|max:250',
-        'remark' => 'required|string|max:250',
+        'remark' => 'nullable|string|max:250',
         'verify_phone_end' => 'required|string|max:250',
     ];
 
