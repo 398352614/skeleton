085b6aac0f3ae8231aa50b0fb75a4005d940ffd9
longshuang <827193289@qq.com>
2020-09-01 11:13:57 +0800

    订单规则修改

---
+++ database/migrations/2020_09_01_105004_create_third_party_log_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThirdPartyLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('third_party_log', function (Blueprint $table) {
+            $table->integerIncrements('id');
+            $table->integer('company_id')->default(null)->nullable()->comment('公司ID');
+            $table->integer('merchant_id')->default(null)->nullable()->comment('商户ID');
+            $table->string('order_no', 50)->default('')->nullable()->comment('订单号');
+            $table->string('content', 10000)->default('')->nullable()->comment('内容');
+            $table->dateTime('created_at')->default(null)->nullable()->comment('创建时间');
+            $table->dateTime('updated_at')->default(null)->nullable()->comment('修改时间');
+
+            $table->index('company_id', 'company_id');
+            $table->index('merchant_id', 'merchant_id');
+            $table->index('order_no', 'order_no');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('third_party_log');
+    }
+}
