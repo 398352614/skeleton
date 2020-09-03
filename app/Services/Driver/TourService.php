00a2d756936ff93261a3dcb8602540c3c389286a
whoyummy <398352614@qq.com>
2020-09-03 11:54:27 +0800

    确认出库

--- app/Services/Driver/TourService.php
+++ app/Services/Driver/TourService.php
@@ -817,7 +817,6 @@ class TourService extends BaseService
         }
         if ($line->can_skip_batch == BaseConstService::CAN_NOT_SKIP_BATCH) {
             $batchList = $this->getBatchService()->getList(['tour_no' => $tour['tour_no'], 'sort_id' => ['<', $batch['sort_id']], 'status' => BaseConstService::TOUR_STATUS_4], ['*'], false);
-            Log::info($batchList);
             if ($batchList->isNotEmpty()) {
                 throw new BusinessLogicException('请按优化的站点顺序进行派送，或手动跳过之前的站点');
             }
