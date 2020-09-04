9f4cb69020205799f48067f33c4de8e3d64d9995
longshuang <827193289@qq.com>
2020-09-04 16:11:56 +0800

    update

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -74,7 +74,7 @@ class TestController extends BaseController
         ];
         $client = new \GuzzleHttp\Client();
         $res = $client->request('POST', $url, [
-            'form_params' => '221'
+            'form_params' => ['reqs' => $list]
         ]);
         $body = $res->getBody();
         $stringBody = (string)$body;
