a34dbddc71c357c44dc3e9186f2e8c2ddf36e5cf
whoyummy <398352614@qq.com>
2020-09-18 18:23:09 +0800

    还原

--- app/Services/Admin/CarService.php
+++ app/Services/Admin/CarService.php
@@ -200,7 +200,6 @@ class CarService extends BaseService
         $data['url_list'] = collect(json_decode($info->relate_material_list))->pluck('url')->toArray();
         foreach ($data['url_list'] as $k => $v) {
             $data['url_list'][$k]=str_replace(env('APP_URL'), storage_path(), $v);
-            dd(storage_path(),$data['url_list'][$k]);
         }
         $url = PrintTrait::tPrint($data, 'car.car', 'car', null);
         return [
