2e9571865cc8a91a2032ce8105029b661c71ed98
whoyummy <398352614@qq.com>
2020-09-18 18:31:51 +0800

    还原

--- app/Services/Admin/CarService.php
+++ app/Services/Admin/CarService.php
@@ -202,7 +202,7 @@ class CarService extends BaseService
         foreach ($data['url_list'] as $k => $v) {
             $data['url_list'][$k] = str_replace(env('APP_URL'), storage_path(), $v);
         }
-        Log::info('info',$data['url_list']);
+        dd($data['url_list']);
         $url = PrintTrait::tPrint($data, 'car.car', 'car', null);
         return [
             'name' => $data['car_no'],
