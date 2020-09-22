b2c59534fcaf2b8cf290900db5fce47712ee1198
whoyummy <398352614@qq.com>
2020-09-22 19:47:35 +0800

    还原

--- app/Services/Admin/HomeService.php
+++ app/Services/Admin/HomeService.php
@@ -276,8 +276,8 @@ class HomeService extends BaseService
             $data[$k]['pickup_order'] = $collection->where('merchant_id', $v['id'])->where('status', '<>', BaseConstService::ORDER_STATUS_7)->where('type', BaseConstService::ORDER_TYPE_1)->count();
             $data[$k]['pie_order'] = $collection->where('merchant_id', $v['id'])->where('status', '<>', BaseConstService::ORDER_STATUS_7)->where('type', BaseConstService::ORDER_TYPE_2)->count();
             $data[$k]['cancel_order'] = $collection->where('merchant_id', $v['id'])->where('status', BaseConstService::ORDER_STATUS_6)->count();
-            $data[$k]['additional_package'] = collect($additionalPackageList)->where('status',BaseConstService::RECHARGE_STATUS_3)->where('merchant_id', $v['id'])->count();
-            $data[$k]['total_recharge'] = $this->getRechargeStatisticsService()->sum('total_recharge_amount', ['merchant_id' => $v['id']]);
+            $data[$k]['additional_package'] = collect($additionalPackageList)->where('merchant_id', $v['id'])->count();
+            $data[$k]['total_recharge'] = $this->getRechargeStatisticsService()->query->where('status',BaseConstService::RECHARGE_STATUS_3)->where('merchant_id',$v['id'])->sum('total_recharge_amount');
             $total['total_order'] += $data[$k]['total_order'];
             $total['pickup_order'] += $data[$k]['pickup_order'];
             $total['pie_order'] += $data[$k]['pie_order'];
