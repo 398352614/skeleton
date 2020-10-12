2b5dce8dc4724c0023c48208708d040f28a1109a
whoyummy <398352614@qq.com>
2020-10-12 18:31:50 +0800

    1

--- app/Services/Admin/TourDriverService.php
+++ app/Services/Admin/TourDriverService.php
@@ -18,7 +18,7 @@ class TourDriverService extends BaseService
 {
     public function __construct(TourDriverEvent $tourDriverEvent)
     {
-        parent::__construct($tourDriverEvent,TourDriverEventResource::class,TourDriverEventResource::class);
+        parent::__construct($tourDriverEvent, TourDriverEventResource::class, TourDriverEventResource::class);
     }
 
 
@@ -28,15 +28,23 @@ class TourDriverService extends BaseService
         foreach ($list as $k => $v) {
             if (!empty($v['batch_no'])) {
                 $batch[$k] = Batch::query()->where('batch_no', $v['batch_no'])->first();
-                $list[$k]['expect_arrive_time']=$batch[$k]['expect_arrive_time'];
-                $list[$k]['actual_arrive_time']=$batch[$k]['actual_arrive_time'];
-                $list[$k]['expect_distance']=$batch[$k]['expect_distance'];
-                $list[$k]['actual_distance']=$batch[$k]['actual_distance'];
-            }else{
-                $list[$k]['expect_arrive_time']='';
-                $list[$k]['actual_arrive_time']='';
-                $list[$k]['expect_distance']='';
-                $list[$k]['actual_distance']='';
+                if (!empty($batch[$k])) {
+                    $list[$k]['expect_arrive_time'] = $batch[$k]['expect_arrive_time'];
+                    $list[$k]['actual_arrive_time'] = $batch[$k]['actual_arrive_time'];
+                    $list[$k]['expect_distance'] = $batch[$k]['expect_distance'];
+                    $list[$k]['actual_distance'] = $batch[$k]['actual_distance'];
+                } else {
+                    $list[$k]['expect_arrive_time'] = '';
+                    $list[$k]['actual_arrive_time'] = '';
+                    $list[$k]['expect_distance'] = '';
+                    $list[$k]['actual_distance'] = '';
+                }
+
+            } else {
+                $list[$k]['expect_arrive_time'] = '';
+                $list[$k]['actual_arrive_time'] = '';
+                $list[$k]['expect_distance'] = '';
+                $list[$k]['actual_distance'] = '';
             }
         }
         return $list;
