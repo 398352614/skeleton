2d5e5bb666be049755fc0455c90341651d110e4a
whoyummy <398352614@qq.com>
2020-11-16 19:02:17 +0800

    1

--- app/Services/Admin/OrderService.php
+++ app/Services/Admin/OrderService.php
@@ -707,6 +707,7 @@ class OrderService extends BaseService
         if(!empty($dbPackageList)){
             $dbPackageList=$dbPackageList->toArray();
             $data['package_list'] = Arr::only($data['package_list'], array_keys($dbPackageList));
+            dd($data['package_list']);
             foreach ($data['package_list'] as $k => $v) {
                 foreach ($v as $x => $y) {
                     if ($y != collect($dbPackageList)->where('express_first_no', $v['express_first_no'])->toArray()[$x]) {
