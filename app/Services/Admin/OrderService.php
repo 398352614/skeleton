6a08ef3e67b502605e2ec731a0e0a3be2a78eb85
whoyummy <398352614@qq.com>
2020-11-16 18:42:51 +0800

    1

--- app/Services/Admin/OrderService.php
+++ app/Services/Admin/OrderService.php
@@ -696,7 +696,7 @@ class OrderService extends BaseService
      */
     public function updateBaseInfo($dbInfo, $data)
     {
-        dd($dbInfo,$data,array_diff($dbInfo,$data));
+        dd($dbInfo,$data);
         $columns = ['special_remark', 'package_list', 'material_list','id'];
         foreach ($data as $k => $v) {
             if (!in_array($k, $columns) && $v !== $dbInfo[$k]) {
