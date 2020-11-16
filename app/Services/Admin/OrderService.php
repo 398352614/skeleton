f2a464374ad7fc20e064542b9f9de21bb8150367
whoyummy <398352614@qq.com>
2020-11-16 18:59:31 +0800

    1

--- app/Services/Admin/OrderService.php
+++ app/Services/Admin/OrderService.php
@@ -706,6 +706,7 @@ class OrderService extends BaseService
         $dbPackageList = $this->getPackageService()->getList(['order_no' => $dbInfo['order_no']], ['*'], false);
         if(!empty($dbPackageList)){
             $dbPackageList=$dbPackageList->toArray();
+            $data['package_list'] = Arr::only($data['package_list'], array_keys($dbPackageList));
             foreach ($data['package_list'] as $k => $v) {
                 foreach ($v as $x => $y) {
                     if ($y != collect($dbPackageList)->where('express_first_no', $v['express_first_no'])->$x) {
@@ -717,6 +718,7 @@ class OrderService extends BaseService
         $dbMaterialList = $this->getMaterialService()->getList(['order_no' => $dbInfo['order_no']], ['*'], false);
         if(!empty($dbMaterialList)){
             $dbMaterialList=$dbMaterialList->toArray();
+            $data['material_list'] = Arr::only($data['material_list'], array_keys($dbMaterialList));
             foreach ($data['material_list'] as $k => $v) {
                 foreach ($v as $x => $y) {
                     if ($y != collect($dbMaterialList)->where('code', $v['code'])->$x) {
