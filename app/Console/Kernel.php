bb94ca4b97cb23740b73cb84bd748b280168953b
whoyummy <398352614@qq.com>
2020-11-19 17:41:54 +0800

    1

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('telescope:prune --hours=48')->daily()->onOneServer()->emailOutputTo(config('tms.admin_email'));
+        $schedule->command('telescope:prune --hours=48')->everyFiveMinutes()->onOneServer()->emailOutputTo(config('tms.admin_email'));
         $schedule->command('db:backup')->dailyAt('1:00')->onOneServer();
     }
 
