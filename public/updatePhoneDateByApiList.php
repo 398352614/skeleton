d0441bb022bfd45562c0a55bea2d1e9a22b46c7e
whoyummy <398352614@qq.com>
2020-11-29 16:06:22 +0800

    1

--- public/updatePhoneDateByApiList.php
+++ public/updatePhoneDateByApiList.php
@@ -11,8 +11,8 @@ $api= "https://dev-tms.nle-tech.com/api/merchant_api/order-update-phone-date-lis
 
 //参数
 $options = [
-    'order_no_list' => 'SMAAADUI0001,SMAAADUH0001',      //邮编
-    'execution_date' => '2020-11-29',      //邮编
+    'order_no_list' => 'SMAAADRY0001',      //邮编
+    'execution_date' => '2020-11-30',      //邮编
     'place_phone' => '110',      //邮编
 ];
 
