d1e6598e6ec7f8a4a2eaef3a50bb49ef7c368aaf
whoyummy <398352614@qq.com>
2020-12-02 11:49:52 +0800

    1

--- app/Services/Driver/TourService.php
+++ app/Services/Driver/TourService.php
@@ -1176,6 +1176,7 @@ class TourService extends BaseService
         if ($rowCount === false) {
             throw new BusinessLogicException('司机入库失败，请重新操作');
         }
+        Log::info('tour',$tour);
         TourTrait::afterBackWarehouse($tour);
     }
 
