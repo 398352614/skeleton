65521b359335135db9b37b63224a8763b39d48f0
longshuang <827193289@qq.com>
2020-12-03 14:08:05 +0800

    update

--- app/Services/Admin/OrderService.php
+++ app/Services/Admin/OrderService.php
@@ -764,6 +764,16 @@ class OrderService extends BaseService
             throw new BusinessLogicException('操作失败,请重新操作');
         }
         $this->getTrackingOrderService()->destroyByOrderNo($dbOrder['order_no']);
+        //材料清除运单信息
+        $rowCount = $this->getMaterialService()->update(['order_no' => $dbOrder['order_no']], ['tracking_order_no' => '']);
+        if ($rowCount === false) {
+            throw new BusinessLogicException('操作失败,请重新操作');
+        }
+        //包裹清除运单信息
+        $rowCount = $this->getPackageService()->update(['order_no' => $dbOrder['order_no']], ['tracking_order_no' => '', 'status' => BaseConstService::PACKAGE_STATUS_4]);
+        if ($rowCount === false) {
+            throw new BusinessLogicException('操作失败,请重新操作');
+        }
         return 'true';
     }
 
