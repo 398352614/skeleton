d530aa9e92de0258304a8b551a24ab57deb3256d
whoyummy <398352614@qq.com>
2020-12-03 18:52:41 +0800

    use优化

--- app/Jobs/PackagePickOut.php
+++ app/Jobs/PackagePickOut.php
@@ -77,10 +77,7 @@ class PackagePickOut implements ShouldQueue
      */
     public function __construct($packageList)
     {
-        Log::info(5);
         $this->packageList = $packageList;
-        Log::info(6);
-
     }
 
 
@@ -90,13 +87,9 @@ class PackagePickOut implements ShouldQueue
      */
     public function handle()
     {
-        Log::info(1);
         $this->curl = new CurlClient();
-        Log::info(2);
         $notifyType = $this->notifyType();
-        Log::info(3);
         $merchantList = $this->getMerchantList(collect($this->packageList)->pluck('merchant_id')->toArray());
-        Log::info(4);
         if (empty($merchantList)) return true;
         foreach ($merchantList as $merchantId => $packageList) {
             $packageList = collect($this->packageList)->where('merchant_id', $merchantId)->only(self::$columns);
