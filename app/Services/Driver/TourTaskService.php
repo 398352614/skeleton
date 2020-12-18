aa89069f69a3dda9ffd9c2ccecaaabef2e3fdf42
whoyummy <398352614@qq.com>
2020-12-18 11:47:10 +0800

    1

--- app/Services/Driver/TourTaskService.php
+++ app/Services/Driver/TourTaskService.php
@@ -251,7 +251,7 @@ class TourTaskService extends BaseService
      */
     public function getAllInfo()
     {
-        $tour = $this->getInfo(['status' => ['<>', BaseConstService::TOUR_STATUS_5]], ['*'], false, ['updated_at' => 'desc']);
+        $tour = $this->getInfo(['status' => ['<>', BaseConstService::TOUR_STATUS_5]], ['*'], false, ['id' => 'desc']);
         if (empty($tour)) {
             return [];
         }
