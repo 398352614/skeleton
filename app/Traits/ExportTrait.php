ca15911bb5e3aaaea90d9226f2e5778ed69653d3
whoyummy <398352614@qq.com>
2020-12-20 11:35:02 +0800

    1

--- app/Traits/ExportTrait.php
+++ app/Traits/ExportTrait.php
@@ -8,6 +8,7 @@ namespace App\Traits;
 use App\Exceptions\BusinessLogicException;
 use App\Exports\BaseExport;
 use App\Exports\PlanExport;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Facades\Excel;
@@ -62,9 +63,9 @@ trait ExportTrait
             if ($dir == 'plan') {
                 $rowCount = Excel::store(new PlanExport($data, $headings, $name, $dir, $params), $path);
             } else {
-                Log::info('开始时间'.time());
+                Log::info('开始时间'.Carbon::now()->format('Y-m-d'));
                 $rowCount = Excel::store(new BaseExport($data, $headings, $name, $dir), $path);
-                Log::info('结束时间'.time());
+                Log::info('结束时间'.Carbon::now()->format('Y-m-d'));
             }
         } catch (\Exception $ex) {
             throw new BusinessLogicException('表格导出失败，请重新操作');
