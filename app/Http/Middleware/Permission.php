0eab0b678dfa67f95fe4c61946f06206de604f9f
longshuang <827193289@qq.com>
2021-01-14 16:38:26 +0800

    update

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -7,21 +7,27 @@ use Closure;
 
 class Permission
 {
+
     /**
      * Handle an incoming request.
      *
+     * @param
      * @param \Illuminate\Http\Request $request
-     * @param \Closure $next
+     * @param Closure $next
+     * @param null $guard
      * @return mixed
+     * @throws BusinessLogicException
      */
     public function handle($request, Closure $next, $guard = null)
     {
         if ($guard !== 'admin') return $next($request);
 
         $prefix = $request->route()->getPrefix();
-        if (in_array($prefix, ['common', 'upload'])) return $next($request);
+        if (in_array($prefix, ['api/admin/common', 'api/admin/upload'])) return $next($request);
 
         $routeAs = $request->route()->getName();
+        if (empty($routeAs) || $routeAs = 'common') return $next($request);
+
         $isAuth = auth()->user()->can($routeAs);
         if ($isAuth === false) {
             throw new BusinessLogicException('当前用户没有该权限');
