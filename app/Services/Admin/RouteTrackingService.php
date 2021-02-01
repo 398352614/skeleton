91dc356486d6409834c19679836e87e3d7bd47b1
whoyummy <398352614@qq.com>
2021-02-01 18:56:49 +0800

    1

--- app/Services/Admin/RouteTrackingService.php
+++ app/Services/Admin/RouteTrackingService.php
@@ -146,18 +146,10 @@ class RouteTrackingService extends BaseService
      */
     public function index()
     {
-        $where = ['driver_id' => ['<>', null]];
-        //筛选是否在线
-        if (!empty($this->formData['is_online']) && $this->formData['is_online'] == BaseConstService::NO) {
-            $where['status'] = ['<>', BaseConstService::TRACKING_ORDER_STATUS_4];
-        } else {
-            $where['status'] = BaseConstService::TOUR_STATUS_4;
-        }
-        //筛选司机名称
         if (!empty($this->formData['driver_name'])) {
-            $info = $this->getTourService()->getList(array_merge($where, ['driver_name' => ['like', $this->formData['driver_name']]]), ['*'], false)->toArray();
+            $info = $this->getTourService()->getList(['status' => BaseConstService::TOUR_STATUS_4, 'driver_name' => ['like', $this->formData['driver_name']]], ['*'], false)->toArray();
         } else {
-            $info = $this->getTourService()->getList($where, ['*'], false)->toArray();
+            $info = $this->getTourService()->getList(['status' => BaseConstService::TOUR_STATUS_4], ['*'], false)->toArray();
         }
         if (empty($info)) {
             throw new BusinessLogicException('暂无车辆信息');
