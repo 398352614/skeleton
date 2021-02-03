f534d469256aed5d7d061ea10002b0659730ec2c
longshuang <827193289@qq.com>
2021-02-03 11:30:59 +0800

    update

--- app/Services/Admin/HomeService.php
+++ app/Services/Admin/HomeService.php
@@ -258,7 +258,7 @@ class HomeService extends BaseService
     {
         $now = date('Y-m-d');
         $trackingOrderCount = $this->getTrackingOrderService()->count(['execution_date' => $now, 'status' => ['in', [BaseConstService::TRACKING_ORDER_STATUS_1, BaseConstService::TRACKING_ORDER_STATUS_2, BaseConstService::TRACKING_ORDER_STATUS_3]]]);
-        $noOutTrackingOrderCount = $this->getTrackingOrderService()->count(['execution_date' => $now, 'out_status' => 2, 'status' => ['in', 【BaseConstService::TRACKING_ORDER_STATUS_1, BaseConstService::TRACKING_ORDER_STATUS_2, BaseConstService::TRACKING_ORDER_STATUS_3】]]);
+        $noOutTrackingOrderCount = $this->getTrackingOrderService()->count(['execution_date' => $now, 'out_status' => 2, 'status' => ['in', [BaseConstService::TRACKING_ORDER_STATUS_1, BaseConstService::TRACKING_ORDER_STATUS_2, BaseConstService::TRACKING_ORDER_STATUS_3]]]);
         $batchCount = $this->getBatchService()->count(['execution_date' => $now, 'status' => ['in', [BaseConstService::BATCH_WAIT_ASSIGN, BaseConstService::BATCH_ASSIGNED, BaseConstService::BATCH_WAIT_OUT]]]);
         $tourCount = $this->getBatchService()->count(['execution_date' => $now, 'status' => ['in', [BaseConstService::TOUR_STATUS_1, BaseConstService::TOUR_STATUS_2, BaseConstService::TOUR_STATUS_3]]]);
         return [
