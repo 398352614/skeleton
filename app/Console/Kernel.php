7b5fa15545d43038936646f7486585edc7ba7b39
whoyummy <398352614@qq.com>
2021-02-18 15:49:48 +0800

    失败发送

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,8 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('telescope:prune --hours=48')->daily()->onOneServer()->emailOutputTo(config('tms.admin_email'));
-        $schedule->command('db:backup')->dailyAt('1:00')->onOneServer()->emailOutputTo(config('tms.admin_email'));
+        $schedule->command('telescope:prune --hours=48')->daily()->onOneServer()->emailOutputOnFailure(config('tms.admin_email'));
+        $schedule->command('db:backup')->dailyAt('1:00')->onOneServer()->emailOutputOnFailure(config('tms.admin_email'));
         $schedule->command('route:retry')->cron('*/'.BaseConstService::ROUTE_RETRY_INTERVAL_TIME.' * * * *')->onOneServer();
     }
 
