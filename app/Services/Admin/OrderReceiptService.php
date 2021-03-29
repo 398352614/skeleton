a84cfcc6e5b808c5c434f49cdc59fe74981726b5
NiZerin <nizerin98@gmail.com>
2021-03-29 18:07:52 +0800

    3月29号更新
    回单接口完成，其他 Bug 修复

---
+++ app/Services/Admin/OrderReceiptService.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Hunan NLE Network Technology Co., Ltd
+ * User : Zelin Ning(NiZerin)
+ * Date : 3/29/2021
+ * Time : 3:24 PM
+ * Email: nzl199851@gmail.com
+ * Blog : nizer.in
+ * FileName: OrderReceiptService.php
+ */
+
+
+namespace App\Services\Admin;
+
+use App\Http\Resources\Api\Admin\OrderReceiptResource;
+use App\Models\OrderReceipt;
+
+/**
+ * Class OrderReceiptService
+ * @package App\Services\Admin
+ */
+class OrderReceiptService extends BaseService
+{
+    /**
+     * @var \string[][]
+     */
+    public $filterRules = [
+        'order_no' => ['=', 'order_no']
+    ];
+
+    /**
+     * OrderReceiptService constructor.
+     * @param  OrderReceipt  $model
+     * @param  null  $infoResource
+     */
+    public function __construct(OrderReceipt $model, $infoResource = null)
+    {
+        parent::__construct($model, OrderReceiptResource::class, $infoResource);
+    }
+}
