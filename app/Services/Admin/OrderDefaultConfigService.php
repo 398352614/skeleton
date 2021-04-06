7db69c1a7c3ea5042bc0ce7fb93e22d3c581447c
NiZerin <i@nizer.in>
2021-04-06 19:09:11 +0800

    4月6号
    订单默认配置

---
+++ app/Services/Admin/OrderDefaultConfigService.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Hunan NLE Network Technology Co., Ltd
+ * User : Zelin Ning(NiZerin)
+ * Date : 4/6/2021
+ * Time : 6:35 PM
+ * Email: i@nizer.in
+ * Blog : nizer.in
+ * FileName: OrderDefaultConfigService.php
+ */
+
+
+namespace App\Services\Admin;
+
+use App\Http\Resources\Api\Admin\OrderDefaultConfigResource;
+use App\Models\OrderDefaultConfig;
+
+/**
+ * Class OrderDefaultConfigService
+ * @package App\Services\Admin
+ */
+class OrderDefaultConfigService extends BaseService
+{
+    /**
+     * OrderDefaultConfigService constructor.
+     * @param  OrderDefaultConfig  $model
+     * @param  null  $infoResource
+     */
+    public function __construct(OrderDefaultConfig $model, $infoResource = null)
+    {
+        parent::__construct($model, OrderDefaultConfigResource::class, $infoResource);
+    }
+}
