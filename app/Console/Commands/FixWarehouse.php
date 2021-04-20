a58808a4816109fef785a3e0b44d2a9dd97cb7cb
whoyummy <398352614@qq.com>
2021-04-20 17:52:14 +0800

    Merge branch 'develop' of github.com:nletech/tms-api into develop
    # Conflicts:
    #	app/Traits/ConstTranslateTrait.php

--- app/Console/Commands/FixWarehouse.php
+++ app/Console/Commands/FixWarehouse.php
@@ -2,6 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Company;
+use App\Models\Employee;
+use App\Models\MapConfig;
+use App\Models\Warehouse;
+use App\Services\BaseConstService;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -41,8 +46,23 @@ class FixWarehouse extends Command
         $this->info('fix begin');
         try {
             //给每个公司新增一个根网点
+            $companyList = Company::query()->get(['id'])->toArray();
+            foreach ($companyList as $company) {
+                $warehouse = MapConfig::query()->where('company_id', $company['id'])->first();
+                if (empty($warehouse)) {
+                    $warehouse = Warehouse::create([
+                        'name' => $company['email'],
+                        'company_id' => $company->id,
+                        'type' => BaseConstService::WAREHOUSE_TYPE_2,
+                        'is_center' => BaseConstService::NO,
+                        'acceptance_type' => BaseConstService::WAREHOUSE_ACCEPTANCE_TYPE_1 . ',' . BaseConstService::WAREHOUSE_ACCEPTANCE_TYPE_2 . ',' . BaseConstService::WAREHOUSE_ACCEPTANCE_TYPE_3,
+                        'line_ids' => '',
+                        'parent_id' => 0
+                    ]);
+                }
+                Employee::query()->where('company_id', $company['id'])->update(['warehouse_id' => $warehouse['id']]);
+            }
             //给每个员工填充根网点
-            //给每个货主填充根网点
         } catch (\Exception $e) {
             $this->info('fix fail:' . $e);
         }
