b9a321760855459e92f280c590774cf7dd3eae31
whoyummy <398352614@qq.com>
2021-05-07 13:39:45 +0800

    m
    # Conflicts:
    #	app/Services/Admin/OrderService.php
    #	app/Traits/ConstTranslateTrait.php

--- app/Services/Admin/LineService.php
+++ app/Services/Admin/LineService.php
@@ -345,7 +345,7 @@ class LineService extends BaseLineService
     {
         $this->query->whereIn('id', $ids);
         if (CompanyTrait::getLineRule() == BaseConstService::LINE_RULE_POST_CODE) {
-            unset($this->formData['post_code']);
+            unset($this->formData);
             return $this->postcodeIndex();
         } else {
             return $this->areaIndex(2);
@@ -361,7 +361,7 @@ class LineService extends BaseLineService
     {
         $this->query->where('warehouse_id', $warehouseId);
         if (CompanyTrait::getLineRule() == BaseConstService::LINE_RULE_POST_CODE) {
-            unset($this->formData['post_code']);
+            unset($this->formData);
             return $this->postcodeIndex();
         } else {
             return $this->areaIndex(2);
