40c5f74a2d767aeffdebda2bbf333ba9ebf6d4a8
whoyummy <398352614@qq.com>
2021-05-07 18:56:56 +0800

    扫描

--- app/Services/Admin/WareHouseService.php
+++ app/Services/Admin/WareHouseService.php
@@ -96,7 +96,6 @@ class WareHouseService extends BaseService
         } else {
             $lineIdList = explode(',', $lineIdList);
         }
-        $data['line_ids'] = $lineIdList;
         $this->check($dbData, $lineIdList);
         $this->fillData($data, $dbData->toArray());
         unset($data['created_at'], $data['updated_at']);
@@ -118,7 +117,7 @@ class WareHouseService extends BaseService
         } elseif (!empty($less)) {
             $lineIdList = array_merge(explode(',', $parentWarehouse['line_ids']), $less);
         } else {
-            return;
+            $lineIdList = [];
         }
         $this->getLineService()->updateWarehouse($dbData['parent'], $lineIdList);
 
