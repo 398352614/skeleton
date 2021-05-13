2faddb7e02e2e3d9adfd85068702bb634ce2343f
whoyummy <398352614@qq.com>
2021-05-13 14:56:25 +0800

    扫描

--- app/Services/Admin/TrackingOrderService.php
+++ app/Services/Admin/TrackingOrderService.php
@@ -970,7 +970,7 @@ class TrackingOrderService extends BaseService
      */
     public function fillWarehouseInfo(&$params, $merchantAlone = BaseConstService::NO)
     {
-        $merchant = $this->getInfo(['id' => $params['merchant_id']]);
+        $merchant = $this->getInfo(['id' => $params['merchant_id']],['*'],false);
         if ($merchant['below_warehouse'] == BaseConstService::YES) {
             $warehouse = $this->getWareHouseService()->getInfo(['id' => $merchant['warehouse_id']], ['*'], false);
         } else {
