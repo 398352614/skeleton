8366d4bb251d3dc4cde9fffd41270feeb05a1241
whoyummy <398352614@qq.com>
2021-05-14 11:23:56 +0800

    扫描

--- app/Services/Admin/DeviceService.php
+++ app/Services/Admin/DeviceService.php
@@ -26,7 +26,6 @@ class DeviceService extends BaseService
     public $filterRules = [
         'number' => ['like', 'keyword'],
         'driver_id' => ['=', 'driver_id'],
-        'driver_name' => ['like', 'driver_name'],
     ];
 
     public function __construct(Device $model)
