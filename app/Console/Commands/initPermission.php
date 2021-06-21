e6a1a3bb47dddcade772276450684a952eac2beb
whoyummy <398352614@qq.com>
2021-06-21 19:38:15 +0800

    m
    # Conflicts:
    #	app/Http/Controllers/Api/Admin/AuthController.php
    #	app/Http/Controllers/Api/Admin/RegisterController.php
    #	app/Http/Controllers/Api/Driver/AuthController.php
    #	app/Traits/ConstTranslateTrait.php
    #	database/migrations/2021_05_22_220624_alter_driver_add_column_timezone.php
    #	resources/lang/cn/validation.php

--- app/Console/Commands/initPermission.php
+++ app/Console/Commands/initPermission.php
@@ -4,15 +4,12 @@ namespace App\Console\Commands;
 
 use App\Models\Company;
 use App\Models\Employee;
-use App\Models\Fee;
 use App\Models\Role;
-use App\Services\BaseConstService;
 use App\Traits\PermissionTrait;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 
 class initPermission extends Command
 {
@@ -20,10 +17,10 @@ class initPermission extends Command
 
     /**
      * The name and signature of the console command.
-     *
+     * mode=1为重置修改，否则为增量修改
      * @var string
      */
-    protected $signature = 'permission:init';
+    protected $signature = 'permission:init {--mode= : mode} {--id= : company id}';
 
     /**
      * The console command description.
@@ -45,22 +42,26 @@ class initPermission extends Command
     /**
      * Execute the console command.
      *
-     * @return mixed
+     * @return void
      */
     public function handle()
     {
         try {
-            $companyList = Company::query()->get(['id'])->toArray();
+            $companyList = empty($this->option('id'))
+                ? Company::query()->get(['id'])->toArray()
+                : Company::query()->where('id', $this->option('id'))->get(['id'])->toArray();
+
             $basePermissionList = self::getPermissionList();
+
             foreach ($companyList as $company) {
                 $this->info($company['id']);
                 $role = Role::query()->where('company_id', $company['id'])->where('is_admin', 1)->first();
                 if (empty($role)) {
                     //新建管理员权限组
                     $role = Role::create([
-                        'company_id' => $company['id'],
-                        'name' => '管理员组',
-                        'is_admin' => 1,
+                        'company_id'    => $company['id'],
+                        'name'          => '管理员组',
+                        'is_admin'      => 1,
                     ]);
                     $employee = Employee::query()->where('company_id', $company['id'])->orderBy('id')->first();
                     if (empty(DB::table('model_has_roles')->where('employee_id', $employee['id'])->first())) {
@@ -69,28 +70,38 @@ class initPermission extends Command
                     }
                 }
                 //给管理员权限组补齐权限
-                $role->syncPermissions(array_column($basePermissionList, 'id'));
+                if ($this->option('mode') == 1) {
+                    //重置修改
+                    $role->syncPermissions($basePermissionList);
+                } else {
+                    //增量修改
+                    $oldPermissionList = collect($role->getAllPermissions())->pluck('id')->toArray();
+                    $addPermissionList = array_diff($basePermissionList, $oldPermissionList);
+                    $role->givePermissionTo($addPermissionList);
+                }
             }
+            $this->info('successful');
         } catch (\Exception $e) {
-            $this->info($e);
+            $this->info($e->getMessage() . "\n");
             $this->info('permission init failed');
-            return;
         }
-        $this->info('successful');
-        return;
     }
 
+    /**
+     * @return mixed
+     */
     public static function getPermissionList()
     {
         $tag = config('tms.cache_tags.permission');
+        Artisan::call('permission:cache');
         $permissionList = Cache::tags($tag)->get('permission_list');
-        if (empty($permissionList)) {
-            Artisan::call('permission:cache');
-            $permissionList = Cache::tags($tag)->get('permission_list');
-        }
-        return $permissionList;
+        return collect($permissionList)->pluck('id')->toArray();
     }
 
+    /**
+     * @param $employee
+     * @param $role
+     */
     protected function addPermission($employee, $role)
     {
         $employee->syncRoles($role);
