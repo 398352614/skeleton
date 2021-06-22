b83b2f385c68e7b341e8dbf28fd95dfde8eb5f40
whoyummy <398352614@qq.com>
2021-06-22 20:44:33 +0800

    m
    # Conflicts:
    #	app/Http/Controllers/Api/Admin/AuthController.php
    #	app/Http/Controllers/Api/Admin/RegisterController.php
    #	app/Http/Controllers/Api/Driver/AuthController.php
    #	app/Traits/ConstTranslateTrait.php
    #	database/migrations/2021_05_22_220624_alter_driver_add_column_timezone.php
    #	resources/lang/cn/validation.php

--- app/Services/ApiServices/GoogleApiService.php
+++ app/Services/ApiServices/GoogleApiService.php
@@ -156,8 +156,8 @@ class GoogleApiService
             'location' => $batchs,
         ];
         app('log')->info('更新线路传送给 api 端的参数为:', $params);
-        Log::info(now('GMT+0100')->timestamp);
-        Log::info(now()->timestamp);
+        Log::info(now()->setTimezone('Europe/Amsterdam'));
+        Log::info(now()->setTimezone('Asia/Shanghai'));
 
         $this->client->postJson($this->url . $api . $this->makeSign(now('GMT+0100')->timestamp), $params);
         FactoryInstanceTrait::getInstance(ApiTimesService::class)->timesCount('api_distance_times', $tour->company_id);
