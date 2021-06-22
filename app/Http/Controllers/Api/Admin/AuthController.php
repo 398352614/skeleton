826315008a317f16b515e3e6867a9d76f6f61958
whoyummy <398352614@qq.com>
2021-06-22 10:16:17 +0800

    m
    # Conflicts:
    #	app/Http/Controllers/Api/Admin/AuthController.php
    #	app/Http/Controllers/Api/Admin/RegisterController.php
    #	app/Http/Controllers/Api/Driver/AuthController.php
    #	app/Traits/ConstTranslateTrait.php
    #	database/migrations/2021_05_22_220624_alter_driver_add_column_timezone.php
    #	resources/lang/cn/validation.php

--- app/Http/Controllers/Api/Admin/AuthController.php
+++ app/Http/Controllers/Api/Admin/AuthController.php
@@ -70,7 +70,7 @@ class AuthController extends Controller
     public function getPermission()
     {
         /**@var Role $role */
-        dd(auth()->user()->roles->first());
+        dd(auth('admin')->user()->roles);
         $role = auth('admin')->user()->roles->first() ?? auth()->user()->roles->first();
         if (empty($role)) return [];
         $rolePermissionList = $role->getAllPermissions();
