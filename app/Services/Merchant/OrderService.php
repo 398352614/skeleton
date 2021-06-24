f604361ba8a50ce0cafa6b5cf9dc7cf95a7979ce
whoyummy <398352614@qq.com>
2021-06-24 19:44:47 +0800

    m
    # Conflicts:
    #	app/Http/Controllers/Api/Admin/AuthController.php
    #	app/Http/Controllers/Api/Admin/RegisterController.php
    #	app/Http/Controllers/Api/Driver/AuthController.php
    #	app/Traits/ConstTranslateTrait.php
    #	database/migrations/2021_05_22_220624_alter_driver_add_column_timezone.php
    #	resources/lang/cn/validation.php

--- app/Services/Merchant/OrderService.php
+++ app/Services/Merchant/OrderService.php
@@ -774,7 +774,7 @@ class OrderService extends BaseService
             $where = ['out_order_no' => $params['out_order_no'], 'status' => ['not in', [BaseConstService::ORDER_STATUS_4, BaseConstService::TRACKING_ORDER_STATUS_5]]];
             !empty($orderNo) && $where['order_no'] = ['<>', $orderNo];
             $dbOrder = parent::getInfo($where, ['id', 'order_no', 'out_order_no', 'status'], false);
-            if (!empty($order)) {
+            if (!empty($dbOrder)) {
                 throw new BusinessLogicException('货号已存在', 1005, [], [
                     'order_no' => $dbOrder['order_no'],
                     'out_order_no' => $dbOrder['out_order_no'] ?? '',
