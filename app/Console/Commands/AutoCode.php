5a0d5572149968fdb003dc9e6d3a463311af6837
whoyummy <398352614@qq.com>
2021-06-29 18:18:39 +0800

    m
    # Conflicts:
    #	app/Http/Controllers/Api/Admin/AuthController.php
    #	app/Http/Controllers/Api/Admin/RegisterController.php
    #	app/Http/Controllers/Api/Driver/AuthController.php
    #	app/Services/Admin/AddressService.php
    #	app/Services/ApiServices/GoogleApiService2.php
    #	app/Services/CurlClient.php
    #	app/Traits/ConstTranslateTrait.php
    #	database/migrations/2021_05_22_220624_alter_driver_add_column_timezone.php
    #	resources/lang/cn/validation.php
    #	routes/api_admin.php

---
+++ app/Console/Commands/AutoCode.php
@@ -0,0 +1,118 @@
+<?php
+
+
+namespace App\Console\Commands;
+
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+
+class AutoCode extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'code';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'auto code';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        try {
+            $this->autoCode();
+            $this->info('The code success.');
+        } catch (\Exception $e) {
+            $this->info('The code fail:' . $e->getMessage());
+        }
+        return;
+    }
+
+    public function autoCode()
+    {
+        //1,正则匹配项目内所有文件以"throw new BusinessException('"开头，以"')"结尾的语句。
+        //2,格式处理
+        //3,写入文件
+        $data = '';
+        $params = file_get_contents('resources/lang/en.json');
+        $params = collect(json_decode($params))->toArray();
+        foreach ($params as $k => $v) {
+            $data .= '"' . $k . '"=>"' . $v . '",'. "\n";
+        }
+        $oldJson = Str::replaceLast('];', '', file_get_contents('app/Exceptions/ErrorCode.php'));
+        $oldJson = Str::replaceLast(']', '', $oldJson);
+        $oldJson = $oldJson . $data . ']' . "\n" . '];';
+        file_put_contents('app/Exceptions/ErrorCode.php', $oldJson);
+        return;
+
+        $data = [];
+        $row = [];
+        $json = '';
+        $tables = DB::connection()->getDoctrineSchemaManager()->listTableNames();
+        foreach ($tables as $k => $v) {
+            $row = array_merge($row, DB::select("SHOW FULL COLUMNS FROM `{$v}`"));
+        }
+        foreach ($row as $k => $v) {
+            $v = collect($v)->toArray();
+            $data[$v['Field']] = $v['Comment'];
+        }
+        foreach ($data as $k => $v) {
+            $data[$k] = explode('1-', $v)[0];
+        }
+        $array = include base_path('resources/lang/cn/validation.php');
+        $attributes = $array['attributes'];
+        $key = array_keys($attributes);
+        $result = Arr::except($data, $key);
+        if (!empty($result)) {
+            foreach ($result as $k => $v) {
+                $json .= '"' . $k . '"=>"' . $v . '",' . "\n";
+            }
+            $oldJson = Str::replaceLast('];', '', file_get_contents('resources/lang/cn/validation.php'));
+            $oldJson = Str::replaceLast(']', '', $oldJson);
+            $oldJson = $oldJson . $json . ']' . "\n" . '];';
+            file_put_contents('resources/lang/cn/validation.php', $oldJson);
+
+
+        }
+        $json = '';
+        $params = [];
+        $array = include base_path('resources/lang/en/validation.php');
+        $diff = Arr::except($data, array_keys($array['attributes']));
+        if (!empty($diff)) {
+            foreach ($diff as $k => $v) {
+                $params[$k] = str_replace('_', ' ', $k);
+            }
+            foreach ($params as $k => $v) {
+                $json .= '"' . $k . '"=>"' . $v . '",' . "\n";
+            }
+            $oldJson = Str::replaceLast('];', '', file_get_contents('resources/lang/en/validation.php'));
+            $oldJson = Str::replaceLast(']', '', $oldJson);
+            $oldJson = $oldJson . $json . ']' . "\n" . '];';
+            file_put_contents('resources/lang/en/validation.php', $oldJson);
+        }
+    }
+}
