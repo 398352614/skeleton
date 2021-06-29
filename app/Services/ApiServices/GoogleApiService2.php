b0157e092ebe05c962b3abd7e773bcdf17bc65d3
whoyummy <398352614@qq.com>
2021-06-29 15:13:36 +0800

    m
    # Conflicts:
    #	app/Http/Controllers/Api/Admin/AuthController.php
    #	app/Http/Controllers/Api/Admin/RegisterController.php
    #	app/Http/Controllers/Api/Driver/AuthController.php
    #	app/Traits/ConstTranslateTrait.php
    #	database/migrations/2021_05_22_220624_alter_driver_add_column_timezone.php
    #	resources/lang/cn/validation.php

--- app/Services/ApiServices/GoogleApiService2.php
+++ app/Services/ApiServices/GoogleApiService2.php
@@ -42,26 +42,26 @@ class GoogleApiService2
         $this->url = config('tms.map_url');
         $this->key = config('tms.map_key');
 
-//        $company = auth('admin')->user();
-//        if (empty($company)) {
-//            $company = auth('merchant')->user();
-//        }
-//        if (empty($company)) {
-//            $company = auth('driver')->user();
-//        }
-//        if (empty(($company))) {
-//            $company = auth()->user();
-//        }
-//        if (empty($company)) {
-//            throw new BusinessLogicException('公司不存在');
-//        }
-//        $mapConfig = MapConfig::query()->where('company_id', $company->company_id)->first();
-//        if (!empty($mapConfig)) {
-//            $this->key = $mapConfig->toArray()['google_key'];
-//        } else {
-//            Log::info('备用Key');
-//            $this->key = config('tms.map_key');
-//        }
+        $company = auth('admin')->user();
+        if (empty($company)) {
+            $company = auth('merchant')->user();
+        }
+        if (empty($company)) {
+            $company = auth('driver')->user();
+        }
+        if (empty(($company))) {
+            $company = auth()->user();
+        }
+        if (empty($company)) {
+            throw new BusinessLogicException('公司不存在');
+        }
+        $mapConfig = MapConfig::query()->where('company_id', $company->company_id)->first();
+        if (!empty($mapConfig)) {
+            $this->key = $mapConfig->toArray()['google_key'];
+        } else {
+            Log::info('备用Key');
+            $this->key = config('tms.map_key');
+        }
         Log::info($this->key);
     }
 
