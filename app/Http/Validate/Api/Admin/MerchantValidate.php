6e81c16710dd6f4549f9dbef8a4b6f620d77e24f
whoyummy <398352614@qq.com>
2021-07-02 17:55:05 +0800

    m
    # Conflicts:
    #	app/Http/Controllers/Api/Admin/AuthController.php
    #	app/Http/Controllers/Api/Admin/RegisterController.php
    #	app/Http/Controllers/Api/Driver/AuthController.php
    #	app/Services/Admin/AddressService.php
    #	app/Services/ApiServices/GoogleApiService2.php
    #	app/Services/CurlClient.php
    #	app/Traits/ConstTranslateTrait.php
    #	database/migrations/2021_05_22_220624_alter_driver_add_column_timezone.php
    #	resources/lang/cn/validation.php
    #	routes/api_admin.php

--- app/Http/Validate/Api/Admin/MerchantValidate.php
+++ app/Http/Validate/Api/Admin/MerchantValidate.php
@@ -27,7 +27,7 @@ class MerchantValidate extends BaseValidate
         'merchant_group_id' => 'required|integer',
         'contacter' => 'required|string|max:50',
         'phone' => 'required|string|max:20|regex:/^[0-9]([0-9-])*[0-9]$/',
-        'address' => 'required|string|max:250',
+        'address' => 'nullable|string|max:250',
         'avatar' => 'nullable|string|max:250',
         'status' => 'required|integer|in:1,2',
         'password' => 'required|string|max:100',
