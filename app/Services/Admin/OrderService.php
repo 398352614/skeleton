fdbb4aa9912c66fd92ead105d200d916890cdfc6
whoyummy <398352614@qq.com>
2021-07-13 15:19:19 +0800

    m
    # Conflicts:
    #	app/Http/Controllers/Api/Admin/AuthController.php
    #	app/Http/Controllers/Api/Admin/RegisterController.php
    #	app/Http/Controllers/Api/Driver/AuthController.php
    #	app/Services/Admin/AddressService.php
    #	app/Services/ApiServices/GoogleApiService2.php
    #	app/Services/CurlClient.php
    #	app/Traits/ConstTranslateTrait.php
    #	database/migrations/2021_05_22_220624_alter_driver_add_column_timezone.php
    #	resources/lang/cn/validation.php
    #	routes/api_admin.php

--- app/Services/Admin/OrderService.php
+++ app/Services/Admin/OrderService.php
@@ -166,9 +166,9 @@ class OrderService extends BaseService
             $list[$k]['exception_label'] = BaseConstService::BATCH_EXCEPTION_LABEL_1;
             $list[$k]['tracking_order_status'] = 0;
             $list[$k]['tracking_order_status_name'] = '';
-            $trackingOrder = $trackingOrderList->where('order_no', $v['order_no'])->sortByDesc('id')->toArray();
+            $trackingOrder = array_values($trackingOrderList->where('order_no', $v['order_no'])->sortByDesc('id')->toArray());
             $list[$k]['tracking_order_count'] = count($trackingOrder);
-            if (!empty($trackingOrder) && !empty($trackingOrder[0])) {
+            if (!empty($trackingOrder)) {
                 $list[$k]['tracking_order_status_name'] = __($trackingOrder[0]['type_name']) . '-' . __($trackingOrder[0]['status_name']);
                 $list[$k]['tracking_order_status'] = $trackingOrder[0]['status'];
                 if ($list[$k]['status'] == BaseConstService::ORDER_STATUS_2 && $trackingOrder[0]['status'] == BaseConstService::TRACKING_ORDER_STATUS_6) {
