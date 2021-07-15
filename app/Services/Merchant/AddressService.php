6435152620487ec230f3750bf61191ad6c1c844b
whoyummy <398352614@qq.com>
2021-07-15 10:53:41 +0800

    m
    # Conflicts:
    #	app/Http/Controllers/Api/Admin/AuthController.php
    #	app/Http/Controllers/Api/Admin/RegisterController.php
    #	app/Http/Controllers/Api/Driver/AuthController.php
    #	app/Services/Admin/AddressService.php
    #	app/Services/ApiServices/GoogleApiService2.php
    #	app/Services/CurlClient.php
    #	app/Traits/ConstTranslateTrait.php
    #	database/migrations/2021_05_22_220624_alter_driver_add_column_timezone.php
    #	resources/lang/cn/validation.php
    #	routes/api_admin.php

--- app/Services/Merchant/AddressService.php
+++ app/Services/Merchant/AddressService.php
@@ -418,6 +418,7 @@ class AddressService extends BaseService
             }
         }
         $address = parent::getInfo($where, ['*'], false);
+        dd($address);
         if (empty($address)) {
             $address = LocationTrait::getLocation($data['place_country'], $data['place_city'], $data['place_street'], $data['place_house_number'], $data['place_post_code']);
             foreach ($array as $k => $v) {
