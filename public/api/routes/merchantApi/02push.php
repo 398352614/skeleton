ca849a99947feb43434ef81b4336e9c035ce4f22
whoyummy <398352614@qq.com>
2021-07-15 15:02:38 +0800

    m
    # Conflicts:
    #	app/Console/Commands/AutoCode.php
    #	app/Http/Controllers/Api/Merchant/AuthController.php
    #	app/Jobs/OutWarehouse.php
    #	app/Listeners/SendNotify2Merchant.php
    #	app/Services/Admin/CarService.php
    #	app/Services/Admin/DriverService.php
    #	app/Services/Admin/EmployeeService.php
    #	app/Services/Admin/TransportPriceService.php
    #	app/Services/Admin/WareHouseService.php
    #	app/Services/Driver/RechargeService.php
    #	app/Services/Driver/RouteTrackingService.php
    #	app/Services/Driver/TourService.php
    #	app/Traits/LocationTrait.php
    #	app/Traits/TourTrait.php
    #	resources/lang/en.json
    #	resources/lang/nl.json

--- public/api/routes/merchantApi/02push.php
+++ public/api/routes/merchantApi/02push.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
  * {
  *       "language": "en"
  *     }
- * @apiDescription 地址栏仅为请求类型，不是真实的推送地址。推送地址由第三方提供，所有的推送都会推送到该地址，但数据格式会由于类型不同而不同。对于请求返回，仅验证返回值中ret是否为1，1表示推送成功。如果返回值中没有ret或者不需要第三方推送记录，那么TMS将不会解析推送后的返回值。
+ * @apiDescription 地址栏仅为请求类型，不是真实的推送地址。推送地址由第三方提供，所有的推送都会推送到第三方提供的地址。目前仅提供简略模式，只推送状态不推送相关数据。对于请求返回，仅验证返回值中ret是否为1，1表示推送成功。如果返回值中没有ret或者不需要第三方推送记录，那么TMS将不会解析推送后的返回值。
  * @apiVersion 1.0.0
  * @apiParam {String} type 类型:签收assign-batch，出库out-warehouse
  * @apiParam {String} data
@@ -30,7 +30,7 @@ use Illuminate\Support\Facades\Route;
  * @apiParam {String} data.package_list.express_first_no 包裹号
  * @apiParam {String} data.package_list.out_order_no 外部包裹号
  * @apiParam {String} data.package_list.stage 包裹阶段1-取件2-派件3-中转
- * @apiParam {String} data.package_list.status 包裹状态分为两种，是包裹阶段的细分过程，其中取件和派件都有以下状态：1-待受理2-已接单3-已装货4-在途5-已签收6-取消7-回收站。中转包裹则为以下状态：1-待装袋2-待装车3-待发车4-运输中5-已到车6-已卸货7-已拆袋
+ * @apiParam {String} data.package_list.status 包裹状态是包裹阶段的细分过程，其中只有取件和派件阶段拥有包裹状态1-未取派2-取派中3-已签收4-取派失败5-回收站。中转阶段的包裹仅显示为最后更新的取件或派件阶段的状态。
  * @apiParamExample {json} Param-Response:
  * {"type":"assign-batch","data":[{"order_no":"TMS0001","out_order_no":"ERP0001","order_status":4,"package_list":[{"express_first_no":"TMSPA001","out_order_no":"ERPPA001","package_status":4,"package_stage":1}]}]}
  * @apiSuccess {Number} ret    状态码，1:成功
