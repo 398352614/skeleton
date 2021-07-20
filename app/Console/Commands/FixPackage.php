1b3e26763a92393283f317aa845d51905fe17710
whoyummy <398352614@qq.com>
2021-07-20 17:47:48 +0800

    m
    # Conflicts:
    #	public/api/doc/merchantApi/api_data.js
    #	public/api/doc/merchantApi/api_data.json
    #	public/api/doc/merchantApi/api_project.js
    #	public/api/doc/merchantApi/api_project.json
    #	public/api/routes/merchantApi/02push.php

--- app/Console/Commands/FixPackage.php
+++ app/Console/Commands/FixPackage.php
@@ -43,9 +43,6 @@ class FixPackage extends Command
     {
         $this->info('begin:' . now());
         try {
-            $trackingPackageList = DB::table('tracking_package')->get();
-            $packageList = DB::table('package')->get();
-
 //            foreach ($trackingPackageList as $k=>$v) {
 //                DB::table('tracking_package')->where('express_first_no',$v->express_first_no)
 //                    ->update(['order_no'=>
@@ -72,15 +69,18 @@ class FixPackage extends Command
                     } elseif (!empty($trackingPackage) && empty($trackingOrder)) {
                         //大条件：只有转运单，则为仓配一体的派件，包裹阶段为中转
                         DB::table('package')->where('express_first_no', $v->express_first_no)->update(['stage' => BaseConstService::PACKAGE_STAGE_2]);
+                        $this->info('fix:' . ($k + 1) . '/' . $count);
                     } elseif (!empty($trackingPackage) && !empty($trackingOrder)) {
                         //大条件：既有运单，也有转运单
                         //装运单的创建时间早于运单的创建时间则，最新创建的是运单
                         if ($trackingPackage->created_at < $trackingOrder->created_at) {
                             //小条件：最新创建的是运单，则包裹阶段为派件
                             DB::table('package')->where('express_first_no', $v->express_first_no)->update(['stage' => BaseConstService::PACKAGE_STAGE_3]);
+                            $this->info('fix:' . ($k + 1) . '/' . $count);
                         } else {
                             //小条件：最新创建的是转运单，则包裹阶段为中转
                             DB::table('package')->where('express_first_no', $v->express_first_no)->update(['stage' => BaseConstService::PACKAGE_STAGE_2]);
+                            $this->info('fix:' . ($k + 1) . '/' . $count);
                         }
                     } else {
                         $this->info('fix fail:' . $v->express_first_no . '包裹阶段空');
