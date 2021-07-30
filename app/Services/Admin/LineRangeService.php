9492015a0fd7e38194b1fd69480bb92c1d6fbb88
whoyummy <398352614@qq.com>
2021-07-30 14:41:00 +0800

    country

--- app/Services/Admin/LineRangeService.php
+++ app/Services/Admin/LineRangeService.php
@@ -53,6 +53,7 @@ class LineRangeService extends BaseService
                     }
                     $schedule[] = $v['schedule'];
                 }
+                $result[$lineId]['line_range'][$country].="\n";
             }
             $newList[$lineId]['line_range'] = array_values($result[$lineId]['line_range']);
             $newList[$lineId]['work_day_list'] = array_values($result[$lineId]['work_day_list']);
--- app/Services/Admin/LineService.php
+++ app/Services/Admin/LineService.php
@@ -69,7 +69,7 @@ class LineService extends BaseLineService
         $warehouseList = $this->getWareHouseService()->getList(['id' => ['in', $list->pluck('warehouse_id')->toArray()]], ['*'], false)->keyBy('id');
         if (empty($lineRangeList)) return $list;
         foreach ($list as &$line) {
-            $line['line_range'] = $lineRangeList[$line['id']]['line_range']."\n";
+            $line['line_range'] = $lineRangeList[$line['id']]['line_range'];
             $line['work_day_list'] = array_values(array_unique($lineRangeList[$line['id']]['work_day_list']));
             $line['warehouse_name'] = $warehouseList[$line['warehouse_id']]['name'];
         }
