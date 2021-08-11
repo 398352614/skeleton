4af723c83507d5bf3a613ad894276d46bbde8837
whoyummy <398352614@qq.com>
2021-08-11 15:03:23 +0800

    m
    # Conflicts:
    #	.env.example
    #	README.md
    #	app/Console/Commands/Data/FixTrackingOrder.php
    #	app/Console/Commands/Data/InitPermission.php
    #	app/Console/Commands/TestGuzzle.php
    #	app/Console/Kernel.php
    #	app/Http/Controllers/Api/Admin/LineController.php
    #	app/Http/Controllers/Api/Admin/RegisterController.php
    #	app/Http/Controllers/Api/Driver/TourTaskController.php
    #	app/Http/Controllers/Api/Merchant/AddressController.php
    #	app/Http/Resources/Api/Driver/MerchantResource.php
    #	app/Http/Resources/Api/Merchant/MerchantResource.php
    #	app/Http/Validate/Api/Admin/LineValidate.php
    #	app/Jobs/ActualOutWarehouse.php
    #	app/Jobs/PackagePickOut.php
    #	app/Jobs/SendPackageInfo.php
    #	app/Jobs/SyncOrderStatus.php
    #	app/Listeners/SendNotify2Merchant.php
    #	app/Listeners/SendOrderCancel.php
    #	app/Listeners/SendOrderDelete.php
    #	app/Listeners/SendOrderExecutionDate.php
    #	app/Services/Admin/BaseLineService.php
    #	app/Services/Admin/LineRangeService.php
    #	app/Services/Admin/LineService.php
    #	app/Services/Admin/OrderService.php
    #	app/Services/Admin/RouteTrackingService.php
    #	app/Services/Admin/TourService.php
    #	app/Services/ApiServices/GoogleApiDistanceService.php
    #	app/Services/ApiServices/GoogleApiService.php
    #	app/Services/ApiServices/TourOptimizationService.php
    #	app/Services/CommonService.php
    #	app/Services/Driver/BaseLineService.php
    #	app/Services/Driver/RechargeService.php
    #	app/Services/Driver/TourService.php
    #	app/Services/Driver/TourTaskService.php
    #	app/Services/Merchant/AddressService.php
    #	app/Services/Merchant/BaseLineService.php
    #	app/Services/Merchant/OrderService.php
    #	app/Traits/DriverServiceTrait.php
    #	app/Traits/LocationTrait.php
    #	app/Traits/UpdateTourTimeAndDistanceTrait.php
    #	config/telescope.php
    #	config/tms.php
    #	database/migrations/old/create/2020_12_10_153408_create_tracking_order_table.php
    #	database/migrations/old/create/2020_12_10_154154_create_tracking_order_package_table.php
    #	database/migrations/old/create/2020_12_10_161152_create_tracking_order_material_table.php
    #	database/migrations/schema.sql
    #	resources/lang/en.json
    #	resources/lang/nl.json
    #	routes/api_admin.php
    #	routes/api_merchant.php
    #	routes/api_merchant_api.php

---
+++ app/Console/Commands/Data/FixCompanyConfig.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands\Data;
+
+use App\Models\Company;
+use App\Models\CompanyConfig;
+use App\Services\BaseConstService;
+use Illuminate\Console\Command;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixCompanyConfig extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fix:company-config';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     */
+    public function handle()
+    {
+        $data = [];
+        $companyList = Company::query()->get(['*'])->toArray();
+        foreach ($companyList as $k => $company) {
+            foreach ($data as $v) {
+                if(empty(CompanyConfig::query()->where('company_id',$v['company_id'])->get())){
+                    CompanyConfig::create([
+                        'company_id' => $company['id'],
+                        'line_rule' => BaseConstService::LINE_RULE_POST_CODE,
+                        'show_type' => BaseConstService::ALL_SHOW,
+                        'address_template_id' => BaseConstService::ADDRESS_TYPE_1,
+                        'stock_exception_verify' => BaseConstService::NO,
+                        'weight_unit' => BaseConstService::WEIGHT_UNIT_TYPE_2,
+                        'currency_unit' => BaseConstService::CURRENCY_UNIT_TYPE_3,
+                        'volume_unit' => BaseConstService::VOLUME_UNIT_TYPE_2,
+                        'map' => 'google',
+                        'scheduling_rule' => BaseConstService::SCHEDULING_TYPE_1
+                    ]);
+                }
+            }
+        }
+        $this->info('fix company config successful!');
+    }
+}
