1e59655f8e4c7c5d8fee9668208c18a60137b5ad
whoyummy <398352614@qq.com>
2021-10-09 10:24:23 +0800

    1

--- database/migrations/2021_08_23_145126_create_bill_verify_table.php
+++ database/migrations/2021_08_23_145126_create_bill_verify_table.php
@@ -19,7 +19,7 @@ class CreateBillVerifyTable extends Migration
             $table->integer('company_id')->default(null)->nullable()->comment('公司ID');
             $table->string('verify_no')->default(null)->nullable()->comment('审核编号');
             $table->tinyInteger('status')->default(1)->nullable()->comment('状态1-未审核2-已审核3-拒绝');
-            $table->tinyInteger('pay_type')->default(1)->nullable()->comment('支付方式1-银行转账2-支票3-现金4-余额');
+            $table->tinyInteger('pay_type')->default(null)->nullable()->comment('支付方式1-银行转账2-支票3-现金4-余额');
             $table->decimal('expect_amount')->default(0)->nullable()->comment('应付金额');
             $table->decimal('actual_amount')->default(0)->nullable()->comment('实付金额');
             $table->string('remark')->default(null)->nullable()->comment('备注');
