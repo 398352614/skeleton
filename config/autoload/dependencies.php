5a92fc067c3f1c5d955da47dd5a5d3ad3b7dd8fd
398352614 <398352614@qq.com>
2022-01-17 11:59:45 +0800

    1master

---
+++ config/autoload/dependencies.php
@@ -0,0 +1,13 @@
+<?php
+// 用于管理 DI 的依赖关系和类对应关系
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+return [
+];
